13dfa71e62944553f984dcf58b527ca7fd7e2ca5
MiloShen <Milo-Shen@outlook.com>
2022-03-25 22:15:25 +0800

    feat: add rust implementation for strStr 2

--- Implement strStr II/Rust/src/solution_1.rs
+++ Implement strStr II/Rust/src/solution_1.rs
@@ -3,23 +3,50 @@ pub fn solution(source: String, target: String) -> i32 {
         return 0;
     }
 
+    // BASE
+    const BASE: u32 = 1000000;
+
+    // Chars to Array
     let s_arr: Vec<char> = source.chars().collect();
     let t_arr: Vec<char> = target.chars().collect();
     let s_len = s_arr.len() as i32;
     let t_len = t_arr.len() as i32;
 
-    for i in 0..s_len - t_len + 1 {
-        let mut is_match = true;
+    // a * 31 ^ m
+    let mut power = 1;
+    for i in 0..t_len {
+        power = (power * 31) % BASE;
+    }
 
-        for j in 0..t_len {
-            if s_arr[(i + j) as usize] != t_arr[j as usize] {
-                is_match = false;
-                break;
+    // target code
+    let mut target_code = 0;
+    for i in 0..t_len {
+        target_code = (target_code * 31 + t_arr[i as usize] as u32) % BASE;
+    }
+
+    let mut hash_code = 0;
+    for i in 0..s_len {
+        // abc + d
+        hash_code = (hash_code * 31 + s_arr[i as usize] as u32) % BASE;
+        if i < t_len - 1 {
+            continue;
+        }
+
+        // abcd - a
+        if i >= t_len {
+            hash_code = hash_code - ((s_arr[(i - t_len) as usize] as u32 * power) % BASE);
+            if hash_code < 0 {
+                hash_code += BASE;
             }
         }
 
-        if is_match {
-            return i as i32;
+        // double check
+        if hash_code == target_code {
+            let pos = i - t_len + 1;
+            let cur_hash_str: String = (&s_arr[pos as usize..(i + 1) as usize]).iter().collect();
+            if cur_hash_str == target {
+                return pos;
+            }
         }
     }
 
