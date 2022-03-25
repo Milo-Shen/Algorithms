48a4554d6102a813f4385a2b944f2ff55681d3a0
MiloShen <Milo-Shen@outlook.com>
2022-03-25 22:15:52 +0800

    feat: revert code for strStr 1

--- Implement strStr/Rust/src/solution_1.rs
+++ Implement strStr/Rust/src/solution_1.rs
@@ -1,4 +1,27 @@
 pub fn solution(source: String, target: String) -> i32 {
-    // todo
-    0
+    if target == String::from("") {
+        return 0;
+    }
+
+    let s_arr: Vec<char> = source.chars().collect();
+    let t_arr: Vec<char> = target.chars().collect();
+    let s_len = s_arr.len() as i32;
+    let t_len = t_arr.len() as i32;
+
+    for i in 0..s_len - t_len + 1 {
+        let mut is_match = true;
+
+        for j in 0..t_len {
+            if s_arr[(i + j) as usize] != t_arr[j as usize] {
+                is_match = false;
+                break;
+            }
+        }
+
+        if is_match {
+            return i as i32;
+        }
+    }
+
+    return -1;
 }
