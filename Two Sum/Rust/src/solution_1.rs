52d1ccdfe875b76c4321b811a0fe934b856cfaa4
MiloShen <Milo-Shen@outlook.com>
2022-03-26 21:03:42 +0800

    feat: enhance rust code for Two Sum

--- Two Sum/Rust/src/solution_1.rs
+++ Two Sum/Rust/src/solution_1.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
 pub fn solution(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut hash: HashMap<i32, i32> = HashMap::new();
     let nums_len = nums.len();
+    let mut hash: HashMap<i32, i32> = HashMap::with_capacity(nums_len);
     for i in 0..nums_len {
         let remain = target - nums[i];
         if let Some(val) = hash.get(&remain) {
