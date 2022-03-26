6e1555b4aa2eb113d69a4f96f5ad7bfcb29ef415
MiloShen <Milo-Shen@outlook.com>
2022-03-26 16:19:51 +0800

    feat: update: update js implementation Palindrome Permutation

--- Palindrome Permutation/Javascript/solution.js
+++ Palindrome Permutation/Javascript/solution.js
@@ -1,10 +1,18 @@
+// https://leetcode-cn.com/problems/valid-palindrome/
+
 function solution(s) {
   if (s === '') return true;
 
   let left = 0;
   let right = s.length - 1;
-  while (left <= right) {
-    if (s.charAt(left) !== s.charAt(right)) {
+  while (left < right) {
+    while (left < right && !is_valid(s[left])) {
+      left++;
+    }
+    while (left < right && !is_valid(s[right])) {
+      right--;
+    }
+    if (left < right && s.charAt(left).toLowerCase() !== s.charAt(right).toLowerCase()) {
       return false;
     }
     left++;
@@ -12,4 +20,8 @@ function solution(s) {
   }
 
   return true;
+}
+
+function is_valid(s) {
+  return /[0-9a-z]/i.test(s);
 }
