0f240b6cae39de274911286e638ea6be8264596e
MiloShen <Milo-Shen@outlook.com>
2022-03-26 19:21:04 +0800

    feat: add js implementation of valid palindrome 2

---
+++ Valid Palindrome II/Javascript/solution.js
@@ -0,0 +1,29 @@
+// https://www.lintcode.com/problem/891/
+
+function solution(s) {
+  if (s === '') {
+    return true;
+  }
+
+  let pair = findDifference(s, 0, s.length - 1);
+  if (pair.left >= pair.right) {
+    return true;
+  }
+
+  return isPalindrome(s, pair.left + 1, pair.right) || isPalindrome(s, pair.left, pair.right - 1);
+}
+
+function findDifference(s, left, right) {
+  while (left < right && s.charAt(left) === s.charAt(right)) {
+    left++;
+    right--;
+  }
+  return { left, right };
+}
+
+function isPalindrome(s, left, right) {
+  let pair = findDifference(s, left, right);
+  return pair.left >= pair.right;
+}
+
+console.log(solution('aba'));
