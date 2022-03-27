359292a52c936d5e53965776d5d9eeca4dffc4e9
MiloShen <Milo-Shen@outlook.com>
2022-03-27 14:03:17 +0800

    feat: add js code for two sum 2

---
+++ Two Sum II/Javascript/solution.js
@@ -0,0 +1,28 @@
+// https://www.lintcode.com/problem/56/
+
+function solution(nums, target) {
+  if (!nums || nums.length === 0) {
+    return [-1, -1];
+  }
+  nums = nums.map((x, index) => ({ index, value: x })).sort((a, b) => a.value - b.value);
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let left_obj = nums[left];
+    let right_obj = nums[right];
+    let sum = left_obj.value + right_obj.value;
+    if (sum > target) {
+      right--;
+    } else if (sum < target) {
+      left++;
+    } else {
+      return [right_obj.index, left_obj.index];
+    }
+  }
+
+  return [-1, -1];
+}
+
+console.log(solution([2, 3, 1], 4));
