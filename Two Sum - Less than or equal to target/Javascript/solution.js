35c34db1da52cce4f15ddad56da0511c80a23d83
MiloShen <Milo-Shen@outlook.com>
2022-03-27 20:39:55 +0800

    feat: add js code for two sum 2

---
+++ Two Sum - Less than or equal to target/Javascript/solution.js
@@ -0,0 +1,21 @@
+function solution(nums, target) {
+  nums = nums.sort((a, b) => a - b);
+
+  let count = 0;
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let sum = nums[left] + nums[right];
+    if (sum > target) {
+      right--;
+    } else {
+      count += right - left;
+      left++;
+    }
+  }
+
+  return count;
+}
+
+console.log(solution([2, 7, 11, 15], 24));
