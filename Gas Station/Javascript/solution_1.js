f6618675c0b9e05e0dba5c2b8225a4db9e1bb7aa
MiloShen <Milo-Shen@outlook.com>
2022-03-27 17:34:48 +0800

    feat: udapte js code

--- Gas Station/Javascript/solution_1.js
+++ Gas Station/Javascript/solution_1.js
@@ -1,11 +1,29 @@
-function solution(gas, cost) {
-  let length = gas.length;
-  let left = 0;
-  while (gas[left] < cost[left]) {
-    left++;
+function solution_1(gas, cost) {
+  if (!gas || !gas.length || !cost || !cost.length || cost.length !== gas.length) {
+    return -1;
   }
-  let remain_oil = 0;
-  for (let i = left; i < length; i++) {
-      remain_oil = remain_oil + gas[i] - cost[i];
+
+  let totalSum = 0;
+  for (let idx = 0; idx < gas.length; idx++) {
+    totalSum += gas[idx] - cost[idx];
   }
+
+  if (totalSum < 0) {
+    return -1;
+  }
+
+  let culSum = 0;
+  let startIdx = 0;
+  for (let idx = 0; idx < gas.length; idx++) {
+    if (culSum + gas[idx] - cost[idx] < 0) {
+      startIdx = idx + 1;
+      culSum = 0;
+    } else {
+      culSum += gas[idx] - cost[idx];
+    }
+  }
+
+  return startIdx;
 }
+
+console.log(solution_1([2, 0, 1, 2, 3, 4, 0], [0, 1, 0, 0, 0, 0, 11]));
