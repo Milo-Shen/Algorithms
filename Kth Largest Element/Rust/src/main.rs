f28a5766d7393eeaada1610eb665fd4d105823f5
MiloShen <Milo-Shen@outlook.com>
2022-03-30 22:03:03 +0800

    feat: add rust code for quick select

--- Kth Largest Element/Rust/src/main.rs
+++ Kth Largest Element/Rust/src/main.rs
@@ -1,3 +1,7 @@
+mod solution_1;
+
 fn main() {
-    println!("Hello, world!");
+    let vec = vec![9, 3, 2, 4, 8];
+    let answer_nums = solution_1::solution(vec, 3);
+    println!("answer_nums = {}", answer_nums);
 }
