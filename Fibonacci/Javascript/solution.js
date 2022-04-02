31c4669adf8eaefca99510e847798b423c3f4735
MiloShen <Milo-Shen@outlook.com>
2022-04-02 21:56:31 +0800

    feat: update code for fibonacci

--- Fibonacci/Javascript/solution.js
+++ Fibonacci/Javascript/solution.js
@@ -1,8 +1,8 @@
 // Recursion
 
 function fibonacci(n) {
-  if (n === 1 || n === 2) {
-    return 1;
+  if (n <= 2) {
+    return n - 1;
   }
 
   return fibonacci(n - 1) + fibonacci(n - 2);
--- Fibonacci/Rust/src/solution_1.rs
+++ Fibonacci/Rust/src/solution_1.rs
@@ -1,6 +1,6 @@
 pub fn fibonacci(n: i32) -> i32 {
-    if n == 1 || n == 2 {
-        return 1;
+    if n <= 2 {
+        return n - 1;
     }
 
     return fibonacci(n - 1) + fibonacci(n - 2);
