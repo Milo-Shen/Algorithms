86247dd8a9c029ca406c68a5ecfcadf7b13c338e
MiloShen <Milo-Shen@outlook.com>
2022-04-02 22:12:32 +0800

    feat: add comments

--- Fibonacci/Javascript/solution.js
+++ Fibonacci/Javascript/solution.js
@@ -1,10 +1,13 @@
 // Recursion
 
+// 1. 递归的定义: 函数接受什么样的参数，返回什么样的值，代表什么样的意思
 function fibonacci(n) {
+  // 3. 递归的出口
   if (n <= 2) {
     return n - 1;
   }
 
+  // 2. 递归的拆解
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
