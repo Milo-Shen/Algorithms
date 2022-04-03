62d4c51777f96eb18fa09a6bf0f17dff6e81547c
MiloShen <Milo-Shen@outlook.com>
2022-04-03 16:26:19 +0800

    feat: add rust code for binary search ( first find )

--- Binary Search ( Stack )/Rust/src/main.rs
+++ Binary Search ( Stack )/Rust/src/main.rs
@@ -2,6 +2,6 @@ mod solution_1;
 
 fn main() {
     let arr = vec![1, 2, 2, 4, 5, 5];
-    let answer = solution_1::find_position(arr, 5);
+    let answer = solution_1::binary_search(arr, 2);
     println!("answer = {:?}", answer);
 }
--- Binary Search ( Stack )/Rust/src/solution_1.rs
+++ Binary Search ( Stack )/Rust/src/solution_1.rs
@@ -1,25 +1,41 @@
 // https://leetcode-cn.com/problems/binary-search/
 // https://www.lintcode.com/problem/457/
 
-pub fn find_position(nums: Vec<i32>, target: i32) -> i32 {
-    return binary_search(&nums, 0, (nums.len() - 1) as i32, target);
-}
-
-fn binary_search(nums: &Vec<i32>, start: i32, end: i32, target: i32) -> i32 {
-    if start > end {
+pub fn binary_search(nums: Vec<i32>, target: i32) -> i32 {
+    let nums_len = nums.len();
+    if nums_len == 0 {
         return -1;
     }
 
-    let middle = (start + end) / 2;
-    let middle_value = nums[middle as usize];
+    let mut start = 0;
+    let mut end = (nums_len - 1) as i32;
 
-    if target == middle_value {
-        return middle;
+    // 要点 1: start + 1 < end
+    // 在二分问题中，最常见的错误就是死循环。而这个模版一定不会出现死循环。为什么呢 ?
+    // 因为我们这边使用了 `start + 1 < end`, 而不是 `start < end` 或者 `start <= end`
+    // 通过 `while` 循环，将区间范围从 `n` 缩小到 `2` （只有 `start` 和 `end` 两个点）。
+    // 在 `start` 和 `end` 中判断是否有解。
+    // 而普通的 `start < end` 或者 `start <= end` 在寻找目标最后一次出现的位置的时候，可能出现死循环。
+    while start + 1 < end {
+        let mid = start + (end - start) / 2;
+        let mid_val = nums[mid as usize];
+
+        if mid_val == target {
+            end = mid;
+        } else if mid_val > target {
+            end = mid
+        } else {
+            start = mid;
+        }
     }
 
-    if target < middle_value {
-        return binary_search(nums, start, middle - 1, target);
+    if nums[start as usize] == target {
+        return start;
     }
 
-    return binary_search(nums, middle + 1, end, target);
+    if nums[end as usize] == target {
+        return end;
+    }
+
+    return -1;
 }
