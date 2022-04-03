e373e266c6d0cad80e343719d748c15c6e8cb9c7
MiloShen <Milo-Shen@outlook.com>
2022-04-03 17:24:09 +0800

    feat: add js code for maximum number in mountain sequence

---
+++ Maximum Number in Mountain Sequence/Javascript/solution.js
@@ -0,0 +1,28 @@
+// https://www.lintcode.com/problem/585/
+// 二分法，判断山脉趋势，按照取数递归左边或者右边即可。
+// 山顶的条件是第一个使得 nums[i] > nums[i + 1] 的 i。
+// 当然也可以反过来，最后一个使得 nums[i] > nums[i - 1] 的 i
+
+// 山脉中的最大值
+function mountain_sequence(nums) {
+  let nums_len = nums.length;
+  if (!nums || nums_len === 0) {
+    return -1;
+  }
+
+  let start = 0;
+  let end = nums_len - 1;
+
+  while (start + 1 < end) {
+    let mid = start + Math.floor((end - start) / 2);
+    if (nums[mid] > nums[mid + 1]) {
+      end = mid;
+    } else {
+      start = mid;
+    }
+  }
+
+  return Math.max(nums[start], nums[end]);
+}
+
+console.log(mountain_sequence([1, 2, 4, 8, 6, 3]));
