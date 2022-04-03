dcf06f7fb9e208c0a64968443dc1a7036f1594de
MiloShen <Milo-Shen@outlook.com>
2022-04-03 11:33:27 +0800

    feat: add js code for binary search

---
+++ Binary Search/Javascript/solution.js
@@ -0,0 +1,25 @@
+function find_position(nums, target) {
+  return binary_search(nums, 0, nums.length - 1, target);
+}
+
+function binary_search(nums, start, end, target) {
+  if (start > end) {
+    return -1;
+  }
+
+  let middle = ~~((start + end) / 2);
+
+  if (target === nums[middle]) {
+    return middle;
+  }
+
+  if (target < nums[middle]) {
+    return binary_search(nums, start, middle - 1, target);
+  }
+
+  if (target > nums[middle]) {
+    return binary_search(nums, middle + 1, end, target);
+  }
+}
+
+console.log(find_position([1, 2, 2, 4, 5, 5], 5));
