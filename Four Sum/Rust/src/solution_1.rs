d1d10c6f71e4be5d900ed55bf9c7c3d3a12ce002
MiloShen <Milo-Shen@outlook.com>
2022-04-05 22:44:27 +0800

    refactor: refactor rust code

--- Four Sum/Rust/src/solution_1.rs
+++ Four Sum/Rust/src/solution_1.rs
@@ -27,36 +27,39 @@ pub fn four_sum(nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
 
             // 进行 two sum 的操作
             let target = target - nums[i] - nums[j];
-            let mut left = j + 1;
-            let mut right = nums_len - 1;
-            while left < right {
-                let two_sum = nums[left] + nums[right];
-
-                if two_sum > target {
-                    right = right - 1;
-                }
-
-                if two_sum < target {
-                    left = left + 1;
-                }
-
-                if two_sum == target {
-                    result.push(vec![nums[i], nums[j], nums[left], nums[right]]);
-                    left = left + 1;
-                    right = right - 1;
-
-                    // 进行 two sum 的去重逻辑
-                    while left < right && nums[left] == nums[left - 1] {
-                        left = left + 1;
-                    }
-
-                    while left < right && nums[right] == nums[right + 1] {
-                        right = right - 1;
-                    }
-                }
-            }
+            two_sum(&nums, (j + 1) as i32, (nums_len - 1) as i32, target, i, j, &mut result);
         }
     }
 
     result
+}
+
+// 进行 two sum 操作
+fn two_sum(nums: &Vec<i32>, mut left: i32, mut right: i32, target: i32, i: usize, j: usize, result: &mut Vec<Vec<i32>>) {
+    while left < right {
+        let two_sum = nums[left as usize] + nums[right as usize];
+
+        if two_sum > target {
+            right = right - 1;
+        }
+
+        if two_sum < target {
+            left = left + 1;
+        }
+
+        if two_sum == target {
+            result.push(vec![nums[i], nums[j], nums[left as usize], nums[right as usize]]);
+            left = left + 1;
+            right = right - 1;
+
+            // 进行 two sum 的去重逻辑
+            while left < right && nums[left as usize] == nums[(left - 1) as usize] {
+                left = left + 1;
+            }
+
+            while left < right && nums[right as usize] == nums[(right + 1) as usize] {
+                right = right - 1;
+            }
+        }
+    }
 }
