d3a197b70417695a00c0e3a42b4524afe074ab2d
MiloShen <Milo-Shen@outlook.com>
2022-04-05 09:28:22 +0800

    feat: update js code for two sum design

---
+++ Two Sum Design/Javascript/solution_2.js
@@ -0,0 +1,80 @@
+class TwoSum {
+  constructor() {
+    this.list = [];
+  }
+
+  add(num) {
+    this.list.push(num);
+    let index = this.list.length - 1;
+    while (index > 0 && this.list[index - 1] > this.list[index]) {
+      this.swap(index);
+    }
+  }
+
+  swap(index) {
+    let temp = this.list[index - 1];
+    this.list[index - 1] = this.list[index];
+    this.list[index] = temp;
+  }
+
+  first_index_of(target) {
+    let start = 0;
+    let end = this.list.length - 1;
+    while (start + 1 < end) {
+      let mid = start + Math.floor((end - start) / 2);
+      if (this.list[mid] < target) {
+        start = mid;
+      } else if (this.list[mid] > target) {
+        end = mid;
+      } else {
+        end = mid;
+      }
+    }
+
+    if (this.list[start] === target) {
+      return start;
+    }
+
+    if (this.list[end] === target) {
+      return end;
+    }
+
+    return -1;
+  }
+
+  last_index_of(target) {
+    let start = 0;
+    let end = this.list.length - 1;
+    while (start + 1 < end) {
+      let mid = start + Math.floor((end - start) / 2);
+      if (this.list[mid] < target) {
+        start = mid;
+      } else if (this.list[mid] > target) {
+        end = mid;
+      } else {
+        start = mid;
+      }
+    }
+
+    if (this.list[end] === target) {
+      return end;
+    }
+
+    if (this.list[start] === target) {
+      return start;
+    }
+
+    return -1;
+  }
+}
+
+let two_sum = new TwoSum();
+two_sum.add(2);
+two_sum.add(1);
+two_sum.add(10);
+two_sum.add(3);
+two_sum.add(3);
+
+let first_index_of = two_sum.first_index_of(3);
+let last_index_of = two_sum.last_index_of(3);
+console.log(`first_index_of = ${first_index_of}`, `last_index_of = ${last_index_of}`);
