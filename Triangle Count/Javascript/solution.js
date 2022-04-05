6b9269b62d902f2f60f1d5602b68d3256b1004fe
MiloShen <Milo-Shen@outlook.com>
2022-04-05 16:53:11 +0800

    feat: add js code for triangle count

---
+++ Triangle Count/Javascript/solution.js
@@ -0,0 +1,34 @@
+function triangle_count(s) {
+  let s_len = s.length;
+  if (!s || s_len < 3) {
+    return -1;
+  }
+
+  s = s.sort((a, b) => a - b);
+
+  let count = 0;
+  for (let i = 2; i < s_len; i++) {
+    count += get_triangle_count(s, i);
+  }
+
+  return count;
+}
+
+function get_triangle_count(s, i) {
+  let left = 0;
+  let right = i - 1;
+  let target = s[i];
+  let count = 0;
+  while (left < right) {
+    let sum = s[left] + s[right];
+    if (sum > target) {
+      count += right - left;
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return count;
+}
+
+console.log(triangle_count([3, 4, 6, 7]));
