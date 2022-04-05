4603ba6268a31df2ea55aa34ea62a4a31135d517
MiloShen <Milo-Shen@outlook.com>
2022-04-05 09:57:12 +0800

    feat: add two sum find js code

--- Two Sum Design/Javascript/solution_2.js
+++ Two Sum Design/Javascript/solution_2.js
@@ -1,6 +1,6 @@
 class TwoSum {
   constructor() {
-    this.list = [];
+    this.hash = [];
   }
 
   add(num) {
@@ -9,80 +9,6 @@ class TwoSum {
     while (index > 0 && this.list[index - 1] > this.list[index]) {
       this.swap(index);
     }
-  }
-
-  swap(index) {
-    let temp = this.list[index - 1];
-    this.list[index - 1] = this.list[index];
-    this.list[index] = temp;
-  }
-
-  first_index_of(target) {
-    let start = 0;
-    let end = this.list.length - 1;
-    while (start + 1 < end) {
-      let mid = start + Math.floor((end - start) / 2);
-      if (this.list[mid] < target) {
-        start = mid;
-      } else if (this.list[mid] > target) {
-        end = mid;
-      } else {
-        end = mid;
-      }
-    }
-
-    if (this.list[start] === target) {
-      return start;
-    }
-
-    if (this.list[end] === target) {
-      return end;
-    }
-
-    return -1;
-  }
-
-  last_index_of(target) {
-    let start = 0;
-    let end = this.list.length - 1;
-    while (start + 1 < end) {
-      let mid = start + Math.floor((end - start) / 2);
-      if (this.list[mid] < target) {
-        start = mid;
-      } else if (this.list[mid] > target) {
-        end = mid;
-      } else {
-        start = mid;
-      }
-    }
-
-    if (this.list[end] === target) {
-      return end;
-    }
-
-    if (this.list[start] === target) {
-      return start;
-    }
-
-    return -1;
-  }
-
-  find(target) {
-    let left = 0;
-    let right = this.list.length - 1;
-    while (left < right) {
-      let sum = this.list[left] + this.list[right];
-
-      if (target > sum) {
-        left++;
-      } else if (target < sum) {
-        right--;
-      } else if (target === sum) {
-        return [left, right];
-      }
-    }
-
-    return [-1, -1];
   }
 }
 
