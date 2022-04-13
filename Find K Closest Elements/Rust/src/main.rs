9da9947dc20b080bae142be9935ef897428b7200
Milo-Shen <Milo-Shen@outlook.com>
2022-04-13 13:16:11 +0800

    feat: add rust code for find k closest elements

--- Find K Closest Elements/Rust/src/main.rs
+++ Find K Closest Elements/Rust/src/main.rs
@@ -1,5 +1,7 @@
 mod solution_1;
 
 fn main() {
-    println!("Hello, world!");
+    let arr = vec![1, 10, 15, 25, 35, 45, 50, 59];
+    let result = solution_1::find_closest_elements(arr, 7, 30);
+    println!("{:?}", result);
 }
