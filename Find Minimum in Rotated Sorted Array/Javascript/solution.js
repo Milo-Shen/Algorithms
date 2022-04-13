77883d32435864e03e2f88c9b1089ac9295a904b
Milo-Shen <Milo-Shen@outlook.com>
2022-04-13 19:47:26 +0800

    feat: add js code for Find Minimum in Rotated Sorted Array

---
+++ Find Minimum in Rotated Sorted Array/Javascript/solution.js
@@ -0,0 +1,25 @@
+// https://www.lintcode.com/problem/159/
+
+function findMin(nums) {
+  let nums_len = nums.length;
+  if (!nums || !nums_len) {
+    return -1;
+  }
+
+  let start = 0;
+  let end = nums_len - 1;
+
+  while (start + 1 < end) {
+    let mid = start + Math.floor((end - start) / 2);
+    if (nums[mid] >= nums[end]) {
+      start = mid;
+    } else {
+      end = mid;
+    }
+  }
+
+  return Math.min(nums[start], nums[end]);
+}
+
+let arr = [4, 5, 6, 7, 0, 1, 2];
+console.log(findMin(arr));
