2085a3e78756c8370d618708159a959077e21ab2
Milo-Shen <Milo-Shen@outlook.com>
2022-04-19 22:33:50 +0800

    feat: update code for build_binary_tree ( rust )

--- Binary Tree Path Sum/Rust/src/lib.rs
+++ Binary Tree Path Sum/Rust/src/lib.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::collections::VecDeque;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode<T> {
@@ -8,7 +9,7 @@ pub struct TreeNode<T> {
     pub right: Option<Rc<RefCell<TreeNode<T>>>>,
 }
 
-impl TreeNode<T> {
+impl<T> TreeNode<T> {
     pub fn new(val: i32) -> Self {
         Self {
             val,
@@ -18,4 +19,14 @@ impl TreeNode<T> {
     }
 }
 
-pub fn build_binary_tree(nums: Vec<Option<i32>>) {}
+pub fn build_binary_tree(nums: Vec<Option<i32>>) -> Option<TreeNode<i32>> {
+    let nums_len = nums.len();
+    if nums_len == 0 {
+        return None;
+    }
+
+    let root: TreeNode<i32> = TreeNode::new(nums[0].unwrap());
+    let queue = VecDeque::from([root]);
+
+    None
+}
