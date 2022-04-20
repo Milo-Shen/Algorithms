53dda79123f1a36b3288ec893b6677f822669fc3
Milo-Shen <Milo-Shen@outlook.com>
2022-04-20 08:29:26 +0800

    feat: add implementation of build_binary_tree

--- Binary Tree Path Sum/Rust/src/lib.rs
+++ Binary Tree Path Sum/Rust/src/lib.rs
@@ -19,14 +19,32 @@ impl<T> TreeNode<T> {
     }
 }
 
-pub fn build_binary_tree(nums: Vec<Option<i32>>) -> Option<TreeNode<i32>> {
+pub fn build_binary_tree(nums: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode<i32>>>> {
     let nums_len = nums.len();
     if nums_len == 0 {
         return None;
     }
 
-    let root: TreeNode<i32> = TreeNode::new(nums[0].unwrap());
-    let queue = VecDeque::from([root]);
+    let root_node: TreeNode<i32> = TreeNode::new(nums[0].unwrap());
+    let root_node_rc = Rc::new(RefCell::new(root_node));
+    let mut queue = VecDeque::from([Rc::clone(&root_node_rc)]);
 
-    None
+    let i = 1;
+    while i < nums_len {
+        if let Some(mut node) = queue.pop_front() {
+            if let Some(num) = nums[i] {
+                let new_node: Rc<RefCell<TreeNode<i32>>> = Rc::new(RefCell::new(TreeNode::new(num)));
+                (*node.borrow_mut()).left = Some(Rc::clone(&new_node));
+                queue.push_back(Rc::clone(&new_node));
+            }
+
+            if let Some(num) = nums[i + 1] {
+                let new_node: Rc<RefCell<TreeNode<i32>>> = Rc::new(RefCell::new(TreeNode::new(num)));
+                (*node.borrow_mut()).right = Some(Rc::clone(&new_node));
+                queue.push_back(Rc::clone(&new_node));
+            }
+        }
+    }
+
+    Some(root_node_rc)
 }
