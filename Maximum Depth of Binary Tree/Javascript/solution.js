4de5c2723464b64a188ad32b6f0de19d919e2041
Milo-Shen <Milo-Shen@outlook.com>
2022-04-22 22:11:38 +0800

    feat: add js code for max depth

---
+++ Maximum Depth of Binary Tree/Javascript/solution.js
@@ -0,0 +1,15 @@
+function max_depth(root) {
+  if (!root) {
+    return 0;
+  }
+
+  let left_depth = max_depth(root.left);
+  let right_depth = max_depth(root.right);
+  return Math.max(left_depth, right_depth) + 1;
+}
+
+// test cases
+const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
+let nums = [1, 2, 3, '#', '#', 4, 5];
+let binaryTree = buildBinaryTree(nums);
+console.log(max_depth(binaryTree));
