6b6e9d43272139594d813bd4669bfa929292b0b8
Milo-Shen <Milo-Shen@outlook.com>
2022-04-22 21:05:57 +0800

    feat: update balanced-binary-tree

--- Balanced Binary Tree/Rust/src/main.rs
+++ Balanced Binary Tree/Rust/src/main.rs
@@ -7,4 +7,6 @@ mod solution;
 fn main() {
     let nums = vec![Some(3), Some(9), Some(20), None, None, Some(15), Some(7)];
     let root_node = build_binary_tree(nums);
+    let answer = solution::is_balanced(root_node);
+    println!("{:?}", answer);
 }
--- Balanced Binary Tree/Rust/src/solution.rs
+++ Balanced Binary Tree/Rust/src/solution.rs
@@ -1,8 +1,31 @@
 use std::rc::Rc;
 use std::cell::RefCell;
-use std::vec;
+use std::cmp::max;
 use Balanced_Binary_Tree::{TreeNode, build_binary_tree};
 
 pub fn is_balanced(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> bool {
-    false
+    let (is_balanced, height) = divide_conquer(root);
+    return is_balanced;
+}
+
+// todo: 这里使用了 clone, 后续可以优化这块
+fn divide_conquer(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> (bool, i32) {
+    match root {
+        Some(node) => {
+            let (is_left_balanced, left_height) = divide_conquer(node.borrow().left.clone());
+            let (is_right_balanced, right_height) = divide_conquer(node.borrow().right.clone());
+            let root_height = max(left_height, right_height) + 1;
+
+            if !is_left_balanced || !is_right_balanced {
+                return (false, root_height);
+            }
+
+            if (left_height - right_height).abs() > 1 {
+                return (false, root_height);
+            }
+
+            return (true, root_height);
+        }
+        None => (true, 0)
+    }
 }
