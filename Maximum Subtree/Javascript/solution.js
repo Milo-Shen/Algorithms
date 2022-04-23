a96267cbb69f707285f593f306664fcb0e2dc8eb
Milo-Shen <Milo-Shen@outlook.com>
2022-04-23 10:25:16 +0800

    feat: update code for max sub tree

--- Maximum Subtree/Javascript/solution.js
+++ Maximum Subtree/Javascript/solution.js
@@ -1,30 +1,33 @@
 // https://www.lintcode.com/problem/628/
 
+let max_node = null;
+
 function findSubtree(root) {
   if (!root) {
     return 0;
   }
 
-  let max_arr = [root];
-  divideConquer(root, max_arr);
-  return max_arr[0].val;
+  divideConquer(root);
+  return max_node;
 }
 
-function divideConquer(root, max) {
+function divideConquer(root) {
   if (!root) {
     return 0;
   }
 
-  let left_sum = divideConquer(root.left, max);
-  let right_sum = divideConquer(root.right, max);
+  let left_sum = divideConquer(root.left);
+  let right_sum = divideConquer(root.right);
   let total = root.val + left_sum + right_sum;
-  if (total > max[0].val) {
-    max.unshift(root);
+  if (!max_node || total > max_node.val) {
+    max_node = root;
   }
+
+  return total;
 }
 
 // test cases
 const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
-let nums = [1, -5, 2, 1, 2, -4, -5];
+let nums = [1, '#', 2];
 let binaryTree = buildBinaryTree(nums);
 console.log(findSubtree(binaryTree));
