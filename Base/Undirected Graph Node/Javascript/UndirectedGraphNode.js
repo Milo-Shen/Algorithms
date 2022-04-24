06d63592e40cff462d2d540c4249b2edb27e6068
MiloShen <Milo-Shen@outlook.com>
2022-04-24 14:50:38 +0800

    feat: add js code to build a undirected graphql node

--- Base/Undirected Graph Node/Javascript/UndirectedGraphNode.js
+++ Base/Undirected Graph Node/Javascript/UndirectedGraphNode.js
@@ -11,13 +11,53 @@ function buildUndirectedGraph(data) {
   if (!data || !data.length) {
     return null;
   }
-  let set = new Set();
-  let root = new UndirectedGraphNode(data[0].label);
+
+  let map = new Map();
+
+  // create the root
+  let label = data[0].label;
+  let root = new UndirectedGraphNode(label);
+  map.set(label, root);
+  buildNodeFromNeighbors(root, data[0].neighbors, map);
+
+  // create other nodes
+  for (let i = 1, len = data.length; i < len; i++) {
+    let label = data[i].label;
+    let node = map.get(label);
+    if (!node) {
+      node = new UndirectedGraphNode(label);
+      map.set(label, node);
+    }
+    buildNodeFromNeighbors(node, data[i].neighbors, map);
+  }
+
+  return root;
+}
+
+function buildNodeFromNeighbors(root, list, map) {
+  for (let i = 0, len = list.length; i < len; i++) {
+    const label = list[i];
+    let node = map.get(label);
+    if (!node) {
+      node = new UndirectedGraphNode(label);
+      map.set(label, node);
+    }
+    root.neighbors.push(node);
+  }
 }
 
 module.exports = {
   UndirectedGraphNode,
 };
 
-let test_data = dataAdapter([1, 2, '4#2', 1, '4#4', 1, 2]);
-console.log(buildUndirectedGraph(test_data));
+console.log(buildUndirectedGraph(dataAdapter([1, 2, '4#2', 1, '4#4', 1, 2])));
+console.log(
+  buildUndirectedGraph(
+    dataAdapter([
+      [2, 4],
+      [1, 3],
+      [2, 4],
+      [1, 3],
+    ]),
+  ),
+);
