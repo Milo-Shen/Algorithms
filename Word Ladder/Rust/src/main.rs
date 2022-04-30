4b7b88133b6ac7dde8e6df7b1c30b5da4b122514
Milo-Shen <Milo-Shen@outlook.com>
2022-04-30 11:42:14 +0800

    feat: init rust code for word ladder

---
+++ Word Ladder/Rust/src/main.rs
@@ -0,0 +1,13 @@
+mod solution_1;
+
+fn main() {
+    let begin_word = String::from("hit");
+    let end_word = String::from("cog");
+    let word_list = vec![
+        String::from("hot"), String::from("dot"), String::from("dog"),
+        String::from("lot"), String::from("log"), String::from("cog"),
+    ];
+
+    let answer = solution_1::ladder_length(begin_word, end_word, word_list);
+    println!("answer = {}", answer);
+}
---
+++ Word Ladder/Rust/src/solution_1.rs
@@ -0,0 +1,6 @@
+pub fn ladder_length(begin_word: String, end_word: String, word_list: Vec<String>) -> i32 {
+    // 对异常进行处理
+    let list_len = word_list.len();
+
+    0
+}
