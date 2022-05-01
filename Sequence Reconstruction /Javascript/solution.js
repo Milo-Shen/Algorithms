2075333399ebed04a02ad94a2dde90d55d515ebb
MiloShen <Milo-Shen@outlook.com>
2022-05-01 20:38:11 +0800

    feat: add js code for Sequence Reconstruction

--- Sequence Reconstruction /Javascript/solution.js
+++ Sequence Reconstruction /Javascript/solution.js
@@ -4,7 +4,26 @@
 function sequenceReconstruction(nums, sequences) {
   // 构建图
   let graph = buildGraph(sequences);
+
+  // 计算每个节点的入度数
   let inDegree = buildInDegree(graph);
+
+  // 计算遍历的路径
+  let topOrder = getTopOrder(inDegree, graph);
+
+  // 判断当前拓扑排序是否是唯一的
+  if (!topOrder || topOrder.length !== nums.length) {
+    return false;
+  }
+
+  // 逐个比对
+  for (let i = 0, len = nums.length; i < len; i++) {
+    if (nums[i] !== topOrder[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 // 构建图
@@ -41,14 +60,56 @@ function buildInDegree(graph) {
 
   // 初始化每个节点的入度数
   for (let neighbors of graph.keys()) {
-    console.log(neighbors);
+    inDegree.set(neighbors, 0);
   }
 
   for (let neighbors of graph.values()) {
     for (let neighbor of neighbors) {
-      // console.log(neighbor);
+      inDegree.set(neighbor, inDegree.get(neighbor) + 1);
     }
   }
+
+  return inDegree;
+}
+
+// 计算一个拓扑序列
+function getTopOrder(inDegree, graph) {
+  let queue = [];
+  let topOrder = [];
+
+  // 遍历过的节点数量
+  let travelCount = 0;
+
+  // 把入度数为 0 的节点放到
+  for (let [node, degree] of inDegree) {
+    if (degree !== 0) continue;
+    queue.push(node);
+  }
+
+  // BFS 节点
+  while (queue.length) {
+    if (queue.length > 1) {
+      return null;
+    }
+
+    let node = queue.shift();
+    topOrder.push(node);
+    travelCount++;
+
+    // 它的邻居节点的入度数 - 1
+    let neighbors = graph.get(node);
+    for (let neighbor of neighbors) {
+      let neighborDegree = inDegree.get(neighbor) - 1;
+      inDegree.set(neighbor, neighborDegree);
+
+      // 如果发现当前节点的入度数为 0, 则重新压入栈中
+      if (neighborDegree === 0) {
+        queue.push(neighbor);
+      }
+    }
+  }
+
+  return graph.size === topOrder.length ? topOrder : null;
 }
 
 // test data
