cefe0b3b7da597e569e857a70de79810a580d92c
MiloShen <Milo-Shen@outlook.com>
2022-05-02 21:37:03 +0800

    feat: add rust code for Alien Dictionary

--- Alien Dictionary/Rust/src/solution.rs
+++ Alien Dictionary/Rust/src/solution.rs
@@ -1,7 +1,7 @@
 // https://www.lintcode.com/problem/892/
 // https://leetcode.cn/problems/Jf1JuT/
 
-use std::cmp::min;
+use std::cmp::{min, Reverse};
 use std::collections::{HashMap, HashSet, BinaryHeap};
 
 pub fn alien_order(words: Vec<String>) -> String {
@@ -60,7 +60,7 @@ fn build_graph(words: &Vec<String>) -> Option<HashMap<char, HashSet<char>>> {
     Some(graph)
 }
 
-fn get_in_degree(graph: &HashMap<char, HashSet<char>>) {
+fn get_in_degree(graph: &HashMap<char, HashSet<char>>) -> HashMap<char, usize> {
     // 存储每个节点的入度数
     let mut in_degree: HashMap<char, usize> = HashMap::new();
 
@@ -76,12 +76,44 @@ fn get_in_degree(graph: &HashMap<char, HashSet<char>>) {
         }
     }
 
-    println!("{:?}", in_degree);
+    in_degree
 }
 
 fn get_topological_sorting(graph: &HashMap<char, HashSet<char>>) -> String {
     // 计算入度
-    get_in_degree(graph);
+    let mut in_degree = get_in_degree(graph);
 
-    String::new()
+    let mut min_heap: BinaryHeap<Reverse<char>> = BinaryHeap::new();
+
+    // 遍历过的节点数量和遍历过的路径
+    let mut travel_path = String::new();
+    let mut travel_count = 0;
+
+    // 把所有入度为 0 的节点压入栈中
+    for (&node, &degree) in &in_degree {
+        if degree == 0 {
+            min_heap.push(Reverse(node));
+        }
+    }
+
+    // BFS 图
+    while !min_heap.is_empty() {
+        let node = min_heap.pop().unwrap().0;
+        travel_path.push(node);
+        travel_count = travel_count + 1;
+
+        // 将当前节点的所有邻居节点的入度数 - 1
+        let neighbors = graph.get(&node).unwrap();
+        for &neighbor in neighbors {
+            let neighbor_degree = *in_degree.get(&neighbor).unwrap() - 1;
+            in_degree.insert(neighbor, neighbor_degree);
+
+            // 如果发现有新的入度为 0 的节点, 则推入栈中
+            if neighbor_degree == 0 {
+                min_heap.push(Reverse(neighbor));
+            }
+        }
+    }
+
+    if travel_count == graph.len() { travel_path } else { String::new() }
 }
