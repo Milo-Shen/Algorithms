aa5bd395deb178b4fd25f3def8b83220d71d53a8
MiloShen <Milo-Shen@outlook.com>
2022-05-02 22:11:43 +0800

    feat: init rust code for sequence reconstruction

--- Sequence Reconstruction /Rust/src/main.rs
+++ Sequence Reconstruction /Rust/src/main.rs
@@ -1,5 +1,8 @@
 mod solution;
 
 fn main() {
-    println!("Hello, world!");
+    let org = vec![1, 2, 3];
+    let seqs = vec![vec![1, 2], vec![1, 3], vec![2, 3]];
+    let answer = solution::sequence_reconstruction(org, seqs);
+    println!("answer = {}", answer);
 }
--- Sequence Reconstruction /Rust/src/solution.rs
+++ Sequence Reconstruction /Rust/src/solution.rs
@@ -1,3 +1,6 @@
 // https://leetcode.cn/problems/sequence-reconstruction/
 // https://www.lintcode.com/problem/605
 
+pub fn sequence_reconstruction(nums: Vec<i32>, sequences: Vec<Vec<i32>>) -> bool {
+    false
+}
