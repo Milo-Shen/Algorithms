0f1d2b8b13015c9811988631cfae100bdbd4085b
Milo-Shen <Milo-Shen@outlook.com>
2022-05-08 10:47:38 +0800

    feat: add js code for Serialize and Deserialize Binary Tree

--- Base/BinaryTree/Javascript/BinaryTree.js
+++ Base/BinaryTree/Javascript/BinaryTree.js
@@ -1,5 +1,9 @@
 const { TreeNode } = require('../../TreeNode/Javascript/TreeNode');
 
+function isValid(data) {
+  return data !== null && data !== undefined;
+}
+
 function buildBinaryTree(nums) {
   let nums_len = nums.length;
   if (!nums || !nums_len) {
@@ -10,12 +14,12 @@ function buildBinaryTree(nums) {
   let queue = [root];
   for (let i = 1; i < nums_len; i += 2) {
     let node = queue.shift();
-    if (nums[i] && nums[i] !== '#') {
+    if (isValid(nums[i]) && nums[i] !== '#') {
       let newNode = new TreeNode(nums[i]);
       node.left = newNode;
       queue.push(newNode);
     }
-    if (nums[i + 1] && nums[i + 1] !== '#') {
+    if (isValid(nums[i + 1]) && nums[i + 1] !== '#') {
       let newNode = new TreeNode(nums[i + 1]);
       node.right = newNode;
       queue.push(newNode);
--- Serialize binary trees/Javascript/solution.js
+++ Serialize binary trees/Javascript/solution.js
@@ -1,1 +1,94 @@
 // https://leetcode.cn/problems/xu-lie-hua-er-cha-shu-lcof/
+
+function serialize(root) {
+  if (!root) {
+    return '';
+  }
+
+  let serializeStr = '';
+  let queue = [root];
+
+  while (queue.length) {
+    let node = queue.shift();
+    if (node) {
+      serializeStr += node.val;
+      queue.push(node.left);
+      queue.push(node.right);
+    } else {
+      serializeStr += '#';
+    }
+  }
+
+  return serializeStr;
+}
+
+function deserialize(str) {
+  return _buildBinaryTree(str.split(''));
+}
+
+function isValid(data) {
+  return data !== null && data !== undefined;
+}
+
+function _buildBinaryTree(nums) {
+  let nums_len = nums.length;
+  if (!nums || !nums_len) {
+    return null;
+  }
+
+  let root = new TreeNode(nums[0]);
+  let queue = [root];
+  for (let i = 1; i < nums_len; i += 2) {
+    let node = queue.shift();
+    if (isValid(nums[i]) && nums[i] !== '#') {
+      let newNode = new TreeNode(nums[i]);
+      node.left = newNode;
+      queue.push(newNode);
+    }
+    if (isValid(nums[i + 1]) && nums[i + 1] !== '#') {
+      let newNode = new TreeNode(nums[i + 1]);
+      node.right = newNode;
+      queue.push(newNode);
+    }
+  }
+
+  return root;
+}
+
+// test cases
+const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
+const { TreeNode } = require('../../Base/TreeNode/Javascript/TreeNode');
+let nums = [
+  4,
+  -7,
+  -3,
+  null,
+  null,
+  -9,
+  -3,
+  9,
+  -7,
+  -4,
+  null,
+  6,
+  null,
+  -6,
+  -6,
+  null,
+  null,
+  0,
+  6,
+  5,
+  null,
+  9,
+  null,
+  null,
+  -1,
+  -4,
+  null,
+  null,
+  null,
+  -2,
+];
+let binaryTree = buildBinaryTree(nums);
+console.log(serialize(deserialize(serialize(binaryTree))));
