f221817f3f694cb10b0094e269f9cd47798ad731
MiloShen <Milo-Shen@outlook.com>
2022-05-08 17:05:47 +0800

    feat: init js code for lowestCommonAncestor

---
+++ Lowest Common Ancestor of a Binary Tree/Javascript/solution.js
@@ -0,0 +1,4 @@
+// https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/
+// https://leetcode.cn/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/
+
+function lowestCommonAncestor(root, p, q) {}
