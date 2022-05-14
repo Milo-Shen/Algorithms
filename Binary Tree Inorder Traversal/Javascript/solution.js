bfc9ad92cf9242cc0e80f50dc2d34a4cb97d2bd4
MiloShen <Milo-Shen@outlook.com>
2022-05-14 20:27:37 +0800

    feat: add js code for binary-tree-inorder-traversal

---
+++ Binary Tree Inorder Traversal/Javascript/solution.js
@@ -0,0 +1,30 @@
+// https://leetcode.cn/problems/binary-tree-inorder-traversal/
+
+function inorderTraversal(root) {
+  let queue = [];
+  let result = [];
+
+  let cur_node = root;
+  while (cur_node) {
+    queue.push(cur_node);
+    cur_node = cur_node.left;
+  }
+
+  while (queue.length) {
+    let cur_node = queue.pop();
+    result.push(cur_node.val);
+    cur_node = cur_node.right;
+    while (cur_node) {
+      queue.push(cur_node);
+      cur_node = cur_node.left;
+    }
+  }
+
+  return result;
+}
+
+// test cases
+const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
+let nums = [1, null, 2, 3];
+let binaryTreeRoot = buildBinaryTree(nums);
+console.log(inorderTraversal(binaryTreeRoot));
