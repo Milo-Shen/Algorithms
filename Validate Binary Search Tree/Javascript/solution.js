08dfcecf1458af7f587afe9ffc8d3b907d4175c8
MiloShen <Milo-Shen@outlook.com>
2022-05-14 20:20:04 +0800

    feat: add js code for validate-binary-search-tree

--- Validate Binary Search Tree/Javascript/solution.js
+++ Validate Binary Search Tree/Javascript/solution.js
@@ -1,9 +1,34 @@
 // https://leetcode.cn/problems/validate-binary-search-tree/
 
-function isValidBST(root) {}
+function isValidBST(root) {
+  return isBST(root).isBST;
+}
+
+function isBST(root) {
+  if (!root) {
+    return { isBST: true, max: -Infinity, min: Infinity };
+  }
+
+  let { isBST: is_left_bst, max: left_max, min: left_min } = isBST(root.left);
+  let { isBST: is_right_bst, max: right_max, min: right_min } = isBST(root.right);
+
+  let isCurBsT = is_left_bst && is_right_bst;
+  let left_valid = root.left ? root.left.val < root.val : true;
+  let right_valid = root.right ? root.right.val > root.val : true;
+
+  let left_max_valid = left_max < root.val;
+  let right_min_valid = right_min > root.val;
+
+  isCurBsT = isCurBsT && left_max_valid && right_min_valid && left_valid && right_valid;
+
+  let max = Math.max(left_max, right_max, root.val);
+  let min = Math.min(left_min, right_min, root.val);
+
+  return { isBST: isCurBsT, max, min };
+}
 
 // test cases
 const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
-let nums = [5, 1, 4, null, null, 3, 6];
+let nums = [5, 4, 6, null, null, 3, 7];
 let binaryTreeRoot = buildBinaryTree(nums);
 console.log(isValidBST(binaryTreeRoot));
