d0b31fc41b82aff291ad6e2263aa3e4027321093
MiloShen <Milo-Shen@outlook.com>
2022-05-14 18:05:05 +0800

    feat: update solution 1 for Validate Binary Tree Nodes

---
+++ Validate Binary Tree Nodes/Javascript/solution_2.js
@@ -0,0 +1,11 @@
+// https://leetcode.cn/problems/validate-binary-tree-nodes/
+
+function validateBinaryTreeNodes(n, leftChild, rightChild) {}
+
+
+
+// test cases
+let n = 4;
+let leftChild = [3, -1, 1, -1];
+let rightChild = [-1, -1, 0, -1];
+console.log(validateBinaryTreeNodes(n, leftChild, rightChild));
