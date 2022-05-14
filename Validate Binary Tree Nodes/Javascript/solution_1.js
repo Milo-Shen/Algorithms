e9ca194385bf589e766c535f8baa901726b51b14
MiloShen <Milo-Shen@outlook.com>
2022-05-14 17:59:53 +0800

    feat: add solution 1 for Validate Binary Tree Nodes

---
+++ Validate Binary Tree Nodes/Javascript/solution_1.js
@@ -0,0 +1,48 @@
+// https://leetcode.cn/problems/validate-binary-tree-nodes/
+
+function validateBinaryTreeNodes(n, leftChild, rightChild) {
+  let treeCount = 0;
+  for (let i = 0; i < n; i++) {
+    let count = treeNodeCount(i, leftChild, rightChild);
+    if (count === -1) {
+      return false;
+    }
+    if (count === n) {
+      return true;
+    }
+    if (count > 1) {
+      treeCount++;
+    }
+  }
+  return treeCount === 1;
+}
+
+function treeNodeCount(node, leftChild, rightChild) {
+  let queue = [node];
+  let node_set = new Set();
+
+  while (queue.length) {
+    let node = queue.shift();
+    if (node_set.has(node)) {
+      return -1;
+    }
+    node_set.add(node);
+    // 遍历左子树和右子树
+    let left_node = leftChild[node];
+    let right_node = rightChild[node];
+    if (left_node !== -1) {
+      queue.push(left_node);
+    }
+    if (right_node !== -1) {
+      queue.push(right_node);
+    }
+  }
+
+  return node_set.size;
+}
+
+// test cases
+let n = 4;
+let leftChild = [3, -1, 1, -1];
+let rightChild = [-1, -1, 0, -1];
+console.log(validateBinaryTreeNodes(n, leftChild, rightChild));
