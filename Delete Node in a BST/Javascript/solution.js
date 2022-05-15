cde8b3f62a42a69c14b8c3fc07cb415a94b06ee3
Milo-Shen <Milo-Shen@outlook.com>
2022-05-15 10:13:56 +0800

    feat: init js code for deleteNode

---
+++ Delete Node in a BST/Javascript/solution.js
@@ -0,0 +1,10 @@
+// https://leetcode.cn/problems/delete-node-in-a-bst/
+// https://lintcode.com/problem/remove-node-in-binary-search-tree/
+// https://www.algolist.net/Data_structures/Binary_search_tree/Removal
+
+function deleteNode(root, key) {}
+
+// test cases
+const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
+let nums = [5, 3, 6, 2, 4, null, 7];
+let binaryTree = buildBinaryTree(nums);
