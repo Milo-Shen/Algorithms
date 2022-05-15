257841eaddcadcaadc3bb499d92fd5eb395feb22
MiloShen <Milo-Shen@outlook.com>
2022-05-15 16:31:27 +0800

    feat: add js code solution 1 for Binary Tree Preorder Traversal

---
+++ Binary Tree Preorder Traversal/Javascript/solution_1.js
@@ -0,0 +1,22 @@
+// https://leetcode.cn/problems/binary-tree-preorder-traversal/
+function preorderTraversal(root) {
+  let result = [];
+  travel(root, result);
+  return result;
+}
+
+function travel(root, nums) {
+  if (!root) {
+    return;
+  }
+
+  nums.push(root.val);
+  travel(root.left, nums);
+  travel(root.right, nums);
+}
+
+// test cases
+const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
+let nums = [1, null, 2, 3];
+let binaryTree = buildBinaryTree(nums);
+console.log(preorderTraversal(binaryTree));
