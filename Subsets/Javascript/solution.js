c13e06d1df66ed8313c049132443614439924d9c
Milo-Shen <Milo-Shen@outlook.com>
2022-05-16 22:23:57 +0800

    feat: add js code for subsets

--- Subsets/Javascript/solution.js
+++ Subsets/Javascript/solution.js
@@ -1,7 +1,35 @@
 // https://www.lintcode.com/problem/17/
 
-function subsets(nums) {}
+function subsets(nums) {
+  let result = [];
+  if (!nums) {
+    return result;
+  }
+
+  nums = nums.sort((a, b) => a - b);
+  dfs(nums, 0, [], result);
+
+  return result;
+}
+
+// 1. 递归的定义
+function dfs(nums, index, subset, result) {
+  // 3. 递归的出口
+  if (index === nums.length) {
+    result.push([...subset]);
+    return;
+  }
+
+  // 2. 递归的拆解
+  // 选 nums[index]
+  subset.push(nums[index]);
+  dfs(nums, index + 1, subset, result);
+
+  // 不选 nums[index]
+  subset.pop();
+  dfs(nums, index + 1, subset, result);
+}
 
 // test cases
-let nums = [0];
+let nums = [];
 console.log(subsets(nums));
