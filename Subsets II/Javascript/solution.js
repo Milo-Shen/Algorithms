4862f09ebc7c5d240e4e02cebe260fa6d65a3d88
Milo-Shen <Milo-Shen@outlook.com>
2022-05-17 21:36:13 +0800

    feat: add js code - solution 1 for subsets-ii

---
+++ Subsets II/Javascript/solution.js
@@ -0,0 +1,45 @@
+// https://www.lintcode.com/problem/18
+// https://leetcode.cn/problems/subsets-ii/
+
+function subsetsWithDup(nums) {
+  let result = [];
+  if (!nums) {
+    return result;
+  }
+
+  let visited = new Set();
+
+  nums = nums.sort((a, b) => a - b);
+  dfs(nums, 0, [], result, visited);
+
+  return result;
+}
+
+// 1. 递归的定义
+function dfs(nums, index, subset, result, visited) {
+  // 3. 递归的出口
+  if (index === nums.length) {
+    let flag = subset.join('-');
+
+    if (visited.has(flag)) {
+      return;
+    }
+
+    visited.add(flag);
+    result.push([...subset]);
+    return;
+  }
+
+  // 2. 递归的拆解
+  // 选 nums[index]
+  subset.push(nums[index]);
+  dfs(nums, index + 1, subset, result, visited);
+
+  // 不选 nums[index]
+  subset.pop();
+  dfs(nums, index + 1, subset, result, visited);
+}
+
+// test cases
+let nums = [1, 2, 2];
+console.log(subsetsWithDup(nums));
