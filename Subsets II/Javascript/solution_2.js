233fff3a9fe90fa8549a2d4724b2dca23d6a88cc
Milo-Shen <Milo-Shen@outlook.com>
2022-05-18 21:15:33 +0800

    feat: update code for subsets

--- Subsets II/Javascript/solution_2.js
+++ Subsets II/Javascript/solution_2.js
@@ -8,18 +8,26 @@ function subsets(nums) {
   }
 
   nums = nums.sort((a, b) => a - b);
-  dfs(nums, 0, [], result);
+
+  let visited = new Set();
+
+  dfs(nums, 0, [], result, visited);
 
   return result;
 }
 
 // 1. 递归的定义
-function dfs(nums, start_index, subset, result) {
-  result.push([...subset]);
+function dfs(nums, start_index, subset, result, visited) {
+  let flag = subset.join('-');
+  if (!visited.has(flag)) {
+    result.push([...subset]);
+  }
+
+  visited.add(flag);
 
   for (let i = start_index; i < nums.length; i++) {
     subset.push(nums[i]);
-    dfs(nums, i + 1, subset, result);
+    dfs(nums, i + 1, subset, result, visited);
     subset.pop();
   }
 }
--- Subsets/Javascript/solution_2.js
+++ Subsets/Javascript/solution_2.js
@@ -8,26 +8,18 @@ function subsets(nums) {
   }
 
   nums = nums.sort((a, b) => a - b);
-
-  let visited = new Set();
-
-  dfs(nums, 0, [], result, visited);
+  dfs(nums, 0, [], result);
 
   return result;
 }
 
 // 1. 递归的定义
-function dfs(nums, start_index, subset, result, visited) {
-  let flag = subset.join('-');
-  if (!visited.has(flag)) {
-    result.push([...subset]);
-  }
-
-  visited.add(flag);
+function dfs(nums, start_index, subset, result) {
+  result.push([...subset]);
 
   for (let i = start_index; i < nums.length; i++) {
     subset.push(nums[i]);
-    dfs(nums, i + 1, subset, result, visited);
+    dfs(nums, i + 1, subset, result);
     subset.pop();
   }
 }
