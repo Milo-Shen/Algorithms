d7281d67d216853f54a692144d816fb1ac0ce686
Milo-Shen <Milo-Shen@outlook.com>
2022-05-19 08:49:06 +0800

    feat: init Traveling Salesman Problem

---
+++ Permutations/Javascript/solution_2.js
@@ -0,0 +1,29 @@
+// https://www.lintcode.com/problem/17/
+// https://leetcode.cn/problems/subsets/submissions/
+
+function subsets(nums) {
+  let result = [];
+  if (!nums) {
+    return [[]];
+  }
+
+  nums = nums.sort((a, b) => a - b);
+  dfs(nums, 0, [], result);
+
+  return result;
+}
+
+// 1. 递归的定义
+function dfs(nums, start_index, subset, result) {
+  result.push([...subset]);
+
+  for (let i = start_index; i < nums.length; i++) {
+    subset.push(nums[i]);
+    dfs(nums, i + 1, subset, result);
+    subset.pop();
+  }
+}
+
+// test cases
+let nums = [];
+console.log(subsets(nums));
