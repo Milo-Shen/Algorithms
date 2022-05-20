d77ce3e4d31d66b23e142f8b16a05290af57fee5
Milo-Shen <Milo-Shen@outlook.com>
2022-05-20 22:27:23 +0800

    feat: add solution for traveling salesman

--- Traveling Salesman Problem /Javascript/solution.js
+++ Traveling Salesman Problem /Javascript/solution.js
@@ -1,1 +1,37 @@
 // https://www.lintcode.com/problem/816/
+
+function minCost(n, roads) {
+  // 构建图
+  let graph = build_graph(roads, n);
+  let result = { min: Infinity };
+}
+
+function build_graph(roads, n) {
+  let graph = [];
+
+  // 使用邻接矩阵的方式构建图
+  for (let i = 0; i <= n; i++) {
+    let temp = [];
+    for (let j = 0; j <= n; j++) {
+      temp[j] = Infinity;
+    }
+    graph.push(temp);
+  }
+
+  // 开始构建图
+  for (let [a, b, c] of roads) {
+    graph[a][b] = Math.min(graph[a][b], c);
+    graph[a][b] = Math.min(graph[b][a], c);
+  }
+
+  return graph;
+}
+
+// test cases
+let n = 3;
+let tuple = [
+  [1, 2, 1],
+  [2, 3, 2],
+  [1, 3, 3],
+];
+console.log(minCost(n, tuple));
