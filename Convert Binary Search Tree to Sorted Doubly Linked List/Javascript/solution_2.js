09f1c559382f9dd9847d7edf8b339e47ce0cc6a4
MiloShen <Milo-Shen@outlook.com>
2022-05-21 21:02:45 +0800

    feat: add js solution 2 for treeToDoublyList

---
+++ Convert Binary Search Tree to Sorted Doubly Linked List/Javascript/solution_2.js
@@ -0,0 +1,34 @@
+// https://leetcode.cn/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/
+function treeToDoublyList(root) {
+  if (!root) {
+    return null;
+  }
+
+  let arr = [];
+
+  in_order_travel(root, arr);
+  let len = arr.length;
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].left = arr[(i - 1 + len) % len];
+    arr[i].right = arr[(i + 1 + len) % len];
+  }
+
+  return arr[0];
+}
+
+function in_order_travel(root, arr) {
+  if (!root) {
+    return;
+  }
+
+  in_order_travel(root.left, arr);
+  arr.push(root);
+  in_order_travel(root.right, arr);
+}
+
+// test cases
+const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
+let nums = [4, 2, 5, 1, 3];
+let binaryTreeRoot = buildBinaryTree(nums);
+console.log(treeToDoublyList(binaryTreeRoot));
