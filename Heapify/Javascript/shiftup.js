94f9a51cee4f6d5beeb2ca413801b7a2860f3226
Milo-Shen <Milo-Shen@outlook.com>
2022-05-22 11:02:18 +0800

    feat: add js code for heapify ( shiftup )

--- Heapify/Javascript/shiftup.js
+++ Heapify/Javascript/shiftup.js
@@ -1,1 +1,25 @@
 // https://www.lintcode.com/problem/130/
+
+function heapify(A) {
+  for (let i = 0, len = A.length; i < len; i++) {
+    siftup(A, i);
+  }
+}
+
+function siftup(A, k) {
+  while (k !== 0) {
+    let father = ~~((k - 1) / 2);
+    if (A[k] > A[father]) {
+      break;
+    }
+    let temp = A[k];
+    A[k] = A[father];
+    A[father] = temp;
+    k = father;
+  }
+}
+
+let A = [3, 2, 1, 4, 5];
+heapify(A);
+// test cases
+console.log(A);
