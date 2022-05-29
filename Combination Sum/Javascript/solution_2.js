8dee811dd6d8e37d5a0703defd283ddbf660a681
MiloShen <Milo-Shen@outlook.com>
2022-05-29 17:02:42 +0800

    feat: add js solution 2 for combination-sum

--- Combination Sum/Javascript/solution_2.js
+++ Combination Sum/Javascript/solution_2.js
@@ -8,33 +8,32 @@ function combinationSum(candidates, target) {
   }
 
   // 对数据进行去重排序
-  candidates = [...new Set(candidates)];
-  candidates = candidates.sort((a, b) => a - b);
+  candidates = [...new Set(candidates)].sort((a, b) => a - b);
   dfs(candidates, 0, target, [], result);
 
   return result;
 }
 
-// 1. 递归的定义
+// 递归三要素之一: 递归的定义
 function dfs(nums, index, target, subset, result) {
-  // 3. 递归的出口
-  if (index === nums.length) {
-    return;
-  }
-
+  // 递归三要素之三: 递归的出口, 已经找出和为 target 的一组结果
   if (target === 0) {
     result.push([...subset]);
     return;
   }
 
-  // 2. 递归的拆解
-  // 不选当前数, 直接跳过
-  dfs(nums, index + 1, target, subset, result);
+  // 递归要素之二: 递归的拆解, 挑一个数放入 current
+  for (let i = index, len = nums.length; i < len; i++) {
+    // 如果剩余和比当前数字小, 不考虑当前数字之后更大的数字 ( 升序排列 )
+    // 这个检测可以不在这里做, 也可以在下一层调用做
+    if (target < nums[i]) {
+      return;
+    }
 
-  // 选择当前数
-  if (target - nums[index] >= 0) {
-    subset.push(nums[index]);
-    dfs(nums, index, target - nums[index], subset, result);
+    subset.push(nums[i]);
+    // 递归到下一层去选定下一个数字
+    // 这里传入 1 而不是 i + 1, 下一层 dfs 可以重复使用位置的数字
+    dfs(nums, i, target - nums[i], subset, result);
     subset.pop();
   }
 }
