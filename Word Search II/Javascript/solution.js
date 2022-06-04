ac9507386728d753c6cf6fdb32f574e22c384d21
MiloShen <Milo-Shen@outlook.com>
2022-06-04 10:45:36 +0800

    feat: add js solution for findWords 2

--- Word Search II/Javascript/solution.js
+++ Word Search II/Javascript/solution.js
@@ -1,27 +1,107 @@
 // https://www.lintcode.com/problem/132
 // https://leetcode.cn/problems/word-search-ii/
 
-// todo: to finish findWords
-function findWords(board, words) {
-  // 先找到所有单词的前缀
-  let prefixes = find_prefixes(words);
+// 搜索的四个方向
+const directions = [
+  // 向上移动
+  { x: -1, y: 0 },
+  // 向右移动
+  { x: 0, y: 1 },
+  // 向下移动
+  { x: 1, y: 0 },
+  // 向左移动
+  { x: 0, y: -1 },
+];
 
-  return false;
+function findWords(board, words) {
+  // 存储单词的结果
+  let result = new Set();
+
+  // 先找到所有单词的前缀
+  let { prefixes, max_len } = find_prefixes_maxLen(words);
+
+  // 计算字母矩阵的行数和列数
+  let row = board.length;
+  let col = board[0].length;
+
+  // DFS 遍历寻找是否 word list 里的单词能否被找到
+  for (let i = 0, len = board.length; i < len; i++) {
+    for (let j = 0, len_j = board[i].length; j < len_j; j++) {
+      let visited = init_visited(row, col);
+      dfs(prefixes, board, words, max_len, i, j, board[i][j], visited, result);
+    }
+  }
+
+  return [...result];
 }
 
-function find_prefixes(words) {
-  let prefixes = new Set();
-  for (let i = 0, len = words.length; i < len; i++) {}
+// dfs 找到单词 ( 第一步: 递归的定义 )
+function dfs(prefixes, board, words, max_len, x, y, str, visited, result) {
+  // 第三步, 递归的退出条件
+  if (str.length > max_len || !prefixes.has(str)) {
+    return;
+  }
 
-  return prefixes;
+  if (words.find((x) => x === str)) {
+    result.add(str);
+  }
+
+  // 第二步, 递归的拆解
+  for (let i = 0; i < directions.length; i++) {
+    let delta = directions[i];
+    let pos_x = x + delta.x;
+    let pos_y = y + delta.y;
+
+    if (!is_valid(pos_x, pos_y, board, visited)) {
+      continue;
+    }
+
+    visited[pos_x][pos_y] = true;
+    dfs(prefixes, board, words, max_len, pos_x, pos_y, str + board[pos_x][pos_y], visited, result);
+  }
+}
+
+function is_valid(x, y, board, visited) {
+  let row = board.length;
+  let col = board[0].length;
+
+  if (x < 0 || x >= row || y < 0 || y >= col) {
+    return false;
+  }
+
+  return !visited[x][y];
+}
+
+// 找到单词 words 的所有前缀
+function find_prefixes_maxLen(words) {
+  let max_len = -Infinity;
+  let prefixes = new Set();
+  for (let i = 0, len = words.length; i < len; i++) {
+    let word_len = words[i].length;
+    max_len = max_len < word_len ? word_len : max_len;
+    for (let j = 1, len_j = word_len; j <= len_j; j++) {
+      prefixes.add(words[i].slice(0, j));
+    }
+  }
+
+  return { prefixes, max_len };
+}
+
+function init_visited(row, col) {
+  let visited = [];
+
+  for (let i = 0; i < row; i++) {
+    let row_arr = [];
+    for (let j = 0; j < col; j++) {
+      row_arr.push(false);
+    }
+    visited.push(row_arr);
+  }
+
+  return visited;
 }
 
 // test cases
-let board = [
-  ['o', 'a', 'a', 'n'],
-  ['e', 't', 'a', 'e'],
-  ['i', 'h', 'k', 'r'],
-  ['i', 'f', 'l', 'v'],
-];
-let words = ['oath', 'pea', 'eat', 'rain'];
+let board = ['abce', 'sfes', 'adee'];
+let words = ['abceseeefs', 'abceseedasfe'];
 console.log(findWords(board, words));
