d28c2c6b9b20e0efcd9bfdcf83947e1087a4d509
Milo-Shen <Milo-Shen@outlook.com>
2022-06-05 22:18:29 +0800

    feat: add js code for findLadders

--- Word Ladder II/Javascript/solution_1.js
+++ Word Ladder II/Javascript/solution_1.js
@@ -6,7 +6,46 @@ function findLadders(beginWord, endWord, wordList) {
     return 0;
   }
 
-  return [];
+  // 是否已经访问过
+  let result = [];
+  let path = [beginWord];
+  let visited = new Set();
+  let min_path = { min: Infinity };
+
+  dfs(beginWord, endWord, 1, wordList, visited, path, min_path, result);
+
+  return result;
+}
+
+// 步骤 1: 递归的定义
+function dfs(word, endWord, start_index, wordList, visited, path, min_path, result) {
+  if (word === endWord) {
+    let path_len = path.length;
+    if (path_len < min_path.min) {
+      result.length = 0;
+      result.push([...path]);
+    } else if (path_len === min_path.min) {
+      result.push([...path]);
+    }
+    min_path.min = min_path.min > path_len ? path_len : min_path.min;
+  }
+
+  // 获得下一层的单词
+  let next_words_list = get_next_words(wordList, word);
+
+  for (let i = 0, len = next_words_list.length; i < len; i++) {
+    let cur_word = next_words_list[i];
+
+    if (visited.has(cur_word)) {
+      continue;
+    }
+
+    visited.add(cur_word);
+    path.push(cur_word);
+    dfs(cur_word, endWord, start_index + 1, wordList, visited, path, min_path, result);
+    path.pop();
+    visited.delete(cur_word);
+  }
 }
 
 function get_next_words(wordList, word) {
