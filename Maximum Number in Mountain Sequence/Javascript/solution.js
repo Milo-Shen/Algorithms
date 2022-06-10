4b168cc2979804d1300913fca0afa5ca69a4f8ce
MiloShen <Milo-Shen@outlook.com>
2022-06-10 21:34:46 +0800

    feat: update comment for Maximum Number in Mountain Sequence

--- Maximum Number in Mountain Sequence/Javascript/solution.js
+++ Maximum Number in Mountain Sequence/Javascript/solution.js
@@ -3,7 +3,7 @@
 // 山顶的条件是第一个使得 nums[i] > nums[i + 1] 的 i。
 // 当然也可以反过来，最后一个使得 nums[i] > nums[i - 1] 的 i
 
-// 山脉中的最大值
+// 山脉中的最大值 ( 这里是通过 mid 和 mid + 1 来比较 )
 function mountain_sequence(nums) {
   let nums_len = nums.length;
   if (!nums || nums_len === 0) {
