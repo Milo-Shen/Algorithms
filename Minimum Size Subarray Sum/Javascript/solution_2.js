05312087b0a4af89af68674efd541c217b416b3f
Milo-Shen <Milo-Shen@outlook.com>
2022-06-13 22:25:51 +0800

    feat: add todo js code for minimum-size-subarray-sum

--- Minimum Size Subarray Sum/Javascript/solution_2.js
+++ Minimum Size Subarray Sum/Javascript/solution_2.js
@@ -9,6 +9,52 @@ function minSubArrayLen(target, nums) {
   if (!nums || !nums_len) {
     return 0;
   }
+
+  // 获取前缀和
+  let prefix_sum_arr = prefix_sum(nums);
+
+  // 在结果集上进行二分
+  let start = 1;
+  let end = nums_len;
+
+  // 基本的二分法
+  while (start + 1 < end) {
+    let mid = start + ~~((end - start) / 2);
+
+    if (isValid(prefix_sum_arr, target, mid)) {
+      end = mid;
+    } else {
+      start = mid;
+    }
+  }
+
+  // 寻找最短子数组, 所以先验证 start, 再验证 end
+  if (isValid(prefix_sum_arr, target, start)) {
+    return start;
+  }
+
+  if (isValid(prefix_sum_arr, target, end)) {
+    return end;
+  }
+
+  return -1;
+}
+
+// 判断是否存在一个子数组, 满足: ( 子数组之和 >= target ) and 子数组长度 <= length
+function isValid(prefix_sum, target, length) {
+  // todo: finish it
+}
+
+// 构建前缀和
+function prefix_sum(nums) {
+  let prefix_sum_array = [];
+  prefix_sum_array[0] = 0;
+
+  for (let i = 0, len = nums.length; i < len; i++) {
+    prefix_sum_array[i + 1] = prefix_sum_array[i] + nums[i];
+  }
+
+  return prefix_sum_array;
 }
 
 // test cases
