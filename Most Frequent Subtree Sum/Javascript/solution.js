12b5221fe6a7e29a02a465cfc64142af47431657
MiloShen <Milo-Shen@outlook.com>
2022-06-21 22:52:26 +0800

    feat: add js code for findFrequentTreeSum

--- Most Frequent Subtree Sum/Javascript/solution.js
+++ Most Frequent Subtree Sum/Javascript/solution.js
@@ -6,30 +6,45 @@ function findFrequentTreeSum(root) {
     return [];
   }
 
+  let map = new Map();
   let result = {
     list: [],
-    max_len: -Infinity,
+    max_count: -Infinity,
   };
 
-  divideConquer(root, result);
+  divideConquer(root, map);
+
+  let map_list = [...map];
+  for (let i = 0, len = map_list.length; i < len; i++) {
+    let [num, count] = map_list[i];
+    if (result.max_count < count) {
+      result.max_count = count;
+      result.list.length = 0;
+      result.list.push(num);
+    } else if (count === result.max_count) {
+      result.list.push(num);
+    }
+  }
 
   return result.list;
 }
 
-function divideConquer(root, max_list) {
+function divideConquer(root, map) {
   if (!root) {
     return 0;
   }
 
-  let left_sum = divideConquer(root.left, max_list);
-  let right_sum = divideConquer(root.right, max_list);
+  let left_sum = divideConquer(root.left, map);
+  let right_sum = divideConquer(root.right, map);
   let total = root.val + left_sum + right_sum;
+
+  map.set(total, (map.get(total) || 0) + 1);
 
   return total;
 }
 
 // test cases
 const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
-let nums = [5, 2, -3];
+let nums = [5, 2, -5];
 let binaryTreeRoot = buildBinaryTree(nums);
 console.log(findFrequentTreeSum(binaryTreeRoot));
