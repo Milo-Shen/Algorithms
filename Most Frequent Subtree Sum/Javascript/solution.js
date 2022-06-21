9e50802a60a5cd01150188e27bdb5c3402061690
MiloShen <Milo-Shen@outlook.com>
2022-06-21 22:17:45 +0800

    feat: todo findFrequentTreeSum

--- Most Frequent Subtree Sum/Javascript/solution.js
+++ Most Frequent Subtree Sum/Javascript/solution.js
@@ -1,6 +1,32 @@
 // https://leetcode.cn/problems/most-frequent-subtree-sum/
 
-function findFrequentTreeSum(root) {}
+function findFrequentTreeSum(root) {
+  // 异常检测
+  if (!root) {
+    return [];
+  }
+
+  let result = {
+    list: [],
+    max_len: -Infinity,
+  };
+
+  divideConquer(root, result);
+
+  return result.list;
+}
+
+function divideConquer(root, max_list) {
+  if (!root) {
+    return 0;
+  }
+
+  let left_sum = divideConquer(root.left, max_list);
+  let right_sum = divideConquer(root.right, max_list);
+  let total = root.val + left_sum + right_sum;
+
+  return total;
+}
 
 // test cases
 const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
