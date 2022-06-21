b1f30d6fb95ba1049a036410474a11ba8a4f23c6
MiloShen <Milo-Shen@outlook.com>
2022-06-21 21:55:03 +0800

    feat: init most-frequent-subtree-sum

--- Most Frequent Subtree Sum/Javascript/solution.js
+++ Most Frequent Subtree Sum/Javascript/solution.js
@@ -1,1 +1,9 @@
 // https://leetcode.cn/problems/most-frequent-subtree-sum/
+
+function findFrequentTreeSum(root) {}
+
+// test cases
+const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
+let nums = [5, 2, -3];
+let binaryTreeRoot = buildBinaryTree(nums);
+console.log(findFrequentTreeSum(binaryTreeRoot));
