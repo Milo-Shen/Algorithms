7e2b7553f191e86d710f2d214bcd2660ebb9ac45
MiloShen <Milo-Shen@outlook.com>
2022-06-22 22:26:01 +0800

    feat: add js solution 1 for kDistinctCharacters

--- Substring With At Least K Distinct Characters/Javascript/solution_1.js
+++ Substring With At Least K Distinct Characters/Javascript/solution_1.js
@@ -1,8 +1,28 @@
 // https://www.lintcode.com/problem/1375
 
 function kDistinctCharacters(s, k) {
-  // Write your code here
+  // 异常检测
+  let s_len = s.length;
+  if (!s || !s_len) {
+    return 0;
+  }
+
+  let total = 0;
+  for (let i = 0; i < s_len; i++) {
+    for (let j = 0; j < s_len; j++) {
+      let set = new Set();
+      for (let k = i; k <= j; k++) {
+        set.add(s[k]);
+      }
+      if (set.size >= k) {
+        total++;
+      }
+    }
+  }
+
+  return total;
 }
 
-let S = 'abcabcabca';
-let k = 4;
+let S = 'abcabcabcabc';
+let k = 3;
+console.log(kDistinctCharacters(S, k));
