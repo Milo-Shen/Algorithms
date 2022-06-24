281741b39ffd18dfc9bb975cf20138fcf25f39a9
Milo-Shen <Milo-Shen@outlook.com>
2022-06-24 22:23:02 +0800

    feat: add js solution 2 for longest-substring-without-repeating-characters

---
+++ Longest Substring Without Repeating Characters/Javascript/solution_2.js
@@ -0,0 +1,62 @@
+// https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+
+// 使用二分法进行求解
+function lengthOfLongestSubstring(s) {
+  // 异常检测
+  let s_len = s.length;
+  if (s_len <= 1) {
+    return s_len;
+  }
+
+  let left = 0;
+  let right = s_len - 1;
+
+  while (left + 1 < right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (is_valid(s, mid)) {
+      left = mid;
+    } else {
+      right = mid;
+    }
+  }
+
+  if (is_valid(s, right)) {
+    return right;
+  }
+
+  if (is_valid(s, left)) {
+    return left;
+  }
+
+  return 0;
+}
+
+function is_valid(s, num) {
+  let s_len = s.length;
+
+  let left = 0;
+  let right = num - 1;
+
+  while (right < s_len) {
+    let visited = new Set();
+
+    for (let i = left; i <= right; i++) {
+      let char = s[i];
+      visited.add(char);
+    }
+
+    if (visited.size === num) {
+      return true;
+    }
+
+    left++;
+    right++;
+  }
+
+  return false;
+}
+
+// test cases
+let s = ' ';
+console.log(lengthOfLongestSubstring(s));
