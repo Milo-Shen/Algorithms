848244966770a5757d8df622211fe704afda0639
Milo-Shen <Milo-Shen@outlook.com>
2022-06-25 22:54:08 +0800

    feat: add rust code for longest-substring-without-repeating-characters

---
+++ Longest Substring Without Repeating Characters/Rust/src/main.rs
@@ -0,0 +1,7 @@
+mod solution_1;
+
+fn main() {
+    let s = String::from("s");
+    let answer = solution_1::length_of_longest_substring(s);
+    println!("answer = {}",answer)
+}
---
+++ Longest Substring Without Repeating Characters/Rust/src/solution_1.rs
@@ -0,0 +1,61 @@
+// https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+
+use std::collections::HashSet;
+
+pub fn length_of_longest_substring(s: String) -> i32 {
+    // 将 String 转换成 Vec
+    let s_arr: Vec<char> = s.chars().collect();
+
+    // 异常检测
+    let s_len = s_arr.len();
+    if s_len <= 1 {
+        return s_len as i32;
+    }
+
+    // 在结果集上进行二分
+    let mut left = 0;
+    let mut right = s_len;
+
+    while left + 1 < right {
+        let mid = left + (right - left) / 2;
+
+        if is_valid(&s_arr, mid, s_len) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+
+    if is_valid(&s_arr, right, s_len) {
+        return right as i32;
+    }
+
+    if is_valid(&s_arr, left, s_len) {
+        return left as i32;
+    }
+
+    0
+}
+
+fn is_valid(arr: &Vec<char>, num: usize, arr_len: usize) -> bool {
+    let mut left = 0;
+    let mut right = num - 1;
+
+    while right < arr_len {
+        let mut visited: HashSet<char> = HashSet::new();
+
+        for i in left..(right + 1) {
+            let value = arr[i];
+            visited.insert(value);
+        }
+
+        if visited.len() == num {
+            return true;
+        }
+
+        left = left + 1;
+        right = right + 1;
+    }
+
+    return false;
+}
