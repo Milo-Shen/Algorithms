c38edcd3a50fdebb6462b5d68c8f0d01d89ca1ba
MiloShen <Milo-Shen@outlook.com>
2022-06-29 22:20:14 +0800

    feat: init Design Add and Search Words Data Structure

---
+++ Design Add and Search Words Data Structure/Javascript/solution.js
@@ -0,0 +1,23 @@
+// https://leetcode.cn/problems/design-add-and-search-words-data-structure/
+// https://www.lintcode.com/problem/473/
+
+const WordDictionary = function () {};
+
+/**
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function (word) {};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function (word) {};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * const obj = new WordDictionary()
+ * obj.addWord(word)
+ * const param_2 = obj.search(word)
+ */
