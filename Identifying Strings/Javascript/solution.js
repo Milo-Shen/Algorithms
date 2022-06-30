5f9cb92c4d8089da66980afde390870442e18d81
Milo-Shen <Milo-Shen@outlook.com>
2022-06-30 21:28:06 +0800

    feat: add js code for Identifying Strings

--- Identifying Strings/Javascript/solution.js
+++ Identifying Strings/Javascript/solution.js
@@ -3,7 +3,32 @@
 const { Trie } = require('../../Base/Tire/Javascript/Tire');
 
 function shortPrefix(stringArray) {
-  // write your code here
+  let trie = new Trie();
+  let result = [];
+
+  for (let i = 0, len = stringArray.length; i < len; i++) {
+    trie.insert(stringArray[i]);
+  }
+
+  for (let i = 0, len = stringArray.length; i < len; i++) {
+    result[i] = getUniquePrefix(trie.getRoot(), stringArray[i]);
+  }
+
+  return result;
+}
+
+function getUniquePrefix(root, word) {
+  let node = root;
+
+  for (let i = 0, len = word.length; i < len; i++) {
+    let letter = word[i];
+    if (node.prefix_count === 1) {
+      return word.slice(0, i);
+    }
+    node = node.children.get(letter);
+  }
+
+  return word;
 }
 
 // test cases
