0adc72fa5e38fd03012b3e6836f0ca01d6599454
Milo-Shen <Milo-Shen@outlook.com>
2022-06-30 21:20:10 +0800

    feat: init Identifying Strings

---
+++ Identifying Strings/Javascript/solution.js
@@ -0,0 +1,10 @@
+// https://www.lintcode.com/problem/333/
+
+const { Trie } = require('../../Base/Tire/Javascript/Tire');
+
+function shortPrefix(stringArray) {
+  // write your code here
+}
+
+// test cases
+console.log(shortPrefix(['aaa', 'bbc', 'bcd']));
