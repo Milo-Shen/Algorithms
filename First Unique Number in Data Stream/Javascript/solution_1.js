0490daa825d4ed167f22cd874eee7bc1493f8827
MiloShen <Milo-Shen@outlook.com>
2022-07-02 16:19:35 +0800

    feat: add js code for First Unique Number in Data Stream

---
+++ First Unique Number in Data Stream/Javascript/solution_1.js
@@ -0,0 +1,37 @@
+// https://www.lintcode.com/problem/685
+
+function firstUniqueNumber(nums, number) {
+  // 特殊情况处理
+  let nums_len = nums.length;
+  if (!nums || !nums_len) {
+    return -1;
+  }
+
+  let map = new Map();
+  let find_number = false;
+
+  for (let i = 0; i < nums_len; i++) {
+    let num = nums[i];
+    map.set(num, (map.get(num) || 0) + 1);
+    if (num === number) {
+      find_number = true;
+      break;
+    }
+  }
+
+  if (!find_number) {
+    return -1;
+  }
+
+  for (let i = 0; i < nums_len; i++) {
+    if (map.get(nums[i]) === 1) {
+      return nums[i];
+    }
+  }
+
+  return -1;
+}
+
+// test cases
+let nums = [1, 2, 2, 1, 3, 4, 4, 5, 6];
+console.log(firstUniqueNumber(nums, 5));
