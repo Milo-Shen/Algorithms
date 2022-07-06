eefedccc96d55e0ce0d1245e507cf9373251a067
Milo-Shen <Milo-Shen@outlook.com>
2022-07-06 21:33:42 +0800

    feat: init LRU

---
+++ LRU Cache/Javascript/solution.js
@@ -0,0 +1,29 @@
+// https://www.lintcode.com/problem/134
+// https://leetcode.cn/problems/lru-cache/
+// https://leetcode.cn/problems/lru-cache-lcci/
+// https://leetcode.cn/problems/OrIXps/
+
+/**
+ * @param {number} capacity
+ */
+const LRUCache = function (capacity) {};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
