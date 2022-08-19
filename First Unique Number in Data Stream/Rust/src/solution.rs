ce9f8a530a48fcd8c430a771acae070e6ea70ed9
MiloShen <Milo-Shen@outlook.com>
2022-08-19 22:06:49 +0800

    feat: add rust code for first unique number

--- First Unique Number in Data Stream/Rust/src/solution.rs
+++ First Unique Number in Data Stream/Rust/src/solution.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+
 pub fn first_unique_number(nums: Vec<i32>, number: i32) -> i32 {
-    1
+    // 特殊情况处理
+    let nums_len = nums.len();
+    if nums_len == 0 {
+        return -1;
+    }
+
+    let mut map: HashMap<i32, i32> = HashMap::new();
+
+    for num in nums.iter() {
+        let count = *map.get(num).unwrap_or(&0) + 1;
+        map.insert(*num, count);
+        if *num == number {
+            break;
+        }
+    }
+
+    if !map.contains_key(&number) {
+        return -1;
+    }
+
+    for num in nums.iter() {
+        if *map.get(num).unwrap() == 1 {
+            return *num;
+        }
+    }
+
+    -1
 }
