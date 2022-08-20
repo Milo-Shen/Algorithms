5a9400854f4b0bad4ca4221d3ffef2385fba24cf
MiloShen <Milo-Shen@outlook.com>
2022-08-20 21:29:10 +0800

    feat: init rust code for Second Minimum Node In a Binary Tree

---
+++ Second Minimum Node In a Binary Tree/Rust/src/main.rs
@@ -0,0 +1,9 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use BinaryTree::{TreeNode, build_binary_tree};
+
+mod solution_1;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ Second Minimum Node In a Binary Tree/Rust/src/solution_1.rs
@@ -0,0 +1,7 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+use BinaryTree::TreeNode;
+
+pub fn find_second_minimum_value(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> i32 {
+    0
+}
