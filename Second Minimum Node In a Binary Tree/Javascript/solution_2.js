1bac38fd4a095e83055c2ac10c234151a54d618a
MiloShen <Milo-Shen@outlook.com>
2022-08-20 21:17:18 +0800

    feat: add comment for second-minimum-node-in-a-binary-tree

--- Second Minimum Node In a Binary Tree/Javascript/solution_2.js
+++ Second Minimum Node In a Binary Tree/Javascript/solution_2.js
@@ -8,17 +8,21 @@ function helper(root, min) {
     return -1;
   }
 
+  // 如果当前结点值 > 根节点，那么不用再遍历它的子节点，直接返回该值
   if (root.val > min) {
     return root.val;
   }
 
+  // 否则，即当前结点值 == 根节点, 则需要在两棵子树找目标值结点
   let left = helper(root.left, min);
   let right = helper(root.right, min);
 
+  // 如果两棵子树均存在大于最小值的节点，那么返回较小的那一个
   if (left > min && right > min) {
     return Math.min(left, right);
   }
 
+  // 否则，其余情况均返回较大的那一个
   return Math.max(left, right);
 }
 
