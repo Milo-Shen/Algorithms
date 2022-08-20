16084280573986b58a2816cf94240c0ad216511c
MiloShen <Milo-Shen@outlook.com>
2022-08-20 22:15:00 +0800

    feat: add rust code for find_second_minimum_value

--- Second Minimum Node In a Binary Tree/Rust/src/main.rs
+++ Second Minimum Node In a Binary Tree/Rust/src/main.rs
@@ -1,9 +1,12 @@
-use std::rc::Rc;
 use std::cell::RefCell;
-use BinaryTree::{TreeNode, build_binary_tree};
+use std::rc::Rc;
+use BinaryTree::{build_binary_tree, TreeNode};
 
 mod solution_1;
 
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![Some(2), Some(2), Some(5), None, None, Some(5), Some(7)];
+    let root_node = build_binary_tree(nums);
+    let answer = solution_1::find_second_minimum_value(root_node);
+    println!("answer = {:?}", answer);
 }
--- Second Minimum Node In a Binary Tree/Rust/src/solution_1.rs
+++ Second Minimum Node In a Binary Tree/Rust/src/solution_1.rs
@@ -1,7 +1,40 @@
-use std::rc::Rc;
+use std::borrow::BorrowMut;
 use std::cell::RefCell;
+use std::rc::Rc;
 use BinaryTree::TreeNode;
 
 pub fn find_second_minimum_value(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> i32 {
-    0
+    if root.is_none() {
+        return -1;
+    }
+
+    let min = root.as_ref().unwrap().borrow().val;
+
+    let mut list: Vec<i32> = Vec::with_capacity(100);
+    let mut queue = vec![Rc::clone(&root.unwrap())];
+
+    while !queue.is_empty() {
+        let node = queue.remove(0);
+
+        let val = node.borrow().val;
+        list.push(val);
+
+        if node.borrow().left.is_some() {
+            queue.push(Rc::clone(node.borrow().left.as_ref().unwrap()));
+        }
+
+        if node.borrow().right.is_some() {
+            queue.push(Rc::clone(node.borrow().right.as_ref().unwrap()));
+        }
+    }
+
+    list.sort();
+
+    for val in list {
+        if min < val {
+            return val;
+        }
+    }
+
+    -1
 }
