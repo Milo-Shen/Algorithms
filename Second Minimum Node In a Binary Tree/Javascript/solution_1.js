0560372dffe6d0ce21a2cc23c7ea664382da3db5
MiloShen <Milo-Shen@outlook.com>
2022-08-20 20:18:50 +0800

    feat: add solution 1 for second-minimum-node-in-a-binary-tree

---
+++ Second Minimum Node In a Binary Tree/Javascript/solution_1.js
@@ -0,0 +1,36 @@
+// https://leetcode.cn/problems/second-minimum-node-in-a-binary-tree/
+function findSecondMinimumValue(root) {
+  let min = root.val;
+
+  let queue = [root];
+  let temp = [];
+
+  while (queue.length) {
+    let node = queue.shift();
+    temp.push(node.val);
+
+    if (node.left) {
+      queue.push(node.left);
+    }
+
+    if (node.right) {
+      queue.push(node.right);
+    }
+  }
+
+  temp.sort();
+
+  for (let i = 0, len = temp.length; i < len; i++) {
+    if (min < temp[i]) {
+      return temp[i];
+    }
+  }
+
+  return -1;
+}
+
+// test cases
+const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
+let nums = [5, 5, 6];
+let binaryTree = buildBinaryTree(nums);
+console.log(findSecondMinimumValue(binaryTree));
