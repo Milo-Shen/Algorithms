94ed86eb6636c5b46be938d4d926b2ff95fdc546
MiloShen <Milo-Shen@outlook.com>
2022-08-23 21:52:14 +0800

    feat: init rust code for Binary Tree Inorder Traversal

---
+++ Binary Tree Inorder Traversal/Rust/src/main.rs
@@ -0,0 +1,12 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use BinaryTree::{build_binary_tree, TreeNode};
+
+mod solution_1;
+
+fn main() {
+    let nums = vec![Some(2), Some(2), Some(5), None, None, Some(5), Some(7)];
+    let root_node = build_binary_tree(nums);
+    let answer = solution_1::inorder_traversal(root_node);
+    println!("answer = {:?}", answer);
+}
---
+++ Binary Tree Inorder Traversal/Rust/src/solution_1.rs
@@ -0,0 +1,7 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use BinaryTree::TreeNode;
+
+pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> Vec<i32> {
+    vec![]
+}
