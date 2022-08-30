7e754806ebfd15dfd367570cd6c99adbd4f72339
Milo-Shen <Milo-Shen@outlook.com>
2022-08-30 21:39:54 +0800

    feat: add js solution 2 for Plus One

---
+++ Plus One/Javascript/solution_2.js
@@ -0,0 +1,22 @@
+// https://leetcode.cn/problems/plus-one/
+// 下面的算法会有精度溢出的问题
+
+function plusOne(digits) {
+  let coefficient = digits.length - 1;
+
+  for (let i = coefficient; i >= 0; i--) {
+    digits[i]++;
+    digits[i] %= 10;
+
+    if (digits[i] !== 0) {
+      return digits;
+    }
+  }
+
+  digits.unshift(1);
+
+  return digits;
+}
+
+let digits = [1, 9, 9];
+console.log(plusOne(digits));
