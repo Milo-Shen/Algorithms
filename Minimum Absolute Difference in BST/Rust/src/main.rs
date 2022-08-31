85940008cb3a17c626b21f0762ed0fc2a5bf15a7
Milo-Shen <Milo-Shen@outlook.com>
2022-08-31 12:52:44 +0800

    feat: add rust code for Minimum Absolute Difference in BST

--- Minimum Absolute Difference in BST/Rust/src/main.rs
+++ Minimum Absolute Difference in BST/Rust/src/main.rs
@@ -1,9 +1,14 @@
 // https://leetcode.cn/problems/minimum-absolute-difference-in-bst/
+
 use std::cell::RefCell;
 use std::rc::Rc;
+use rust::build_binary_tree;
 
 mod solution_1;
 
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![Some(236), Some(104), Some(701), None, Some(227), None, Some(911)];
+    let root_node = build_binary_tree(nums);
+    let answer = solution_1::get_minimum_difference(root_node);
+    println!("answer = {:?}", answer);
 }
--- Minimum Absolute Difference in BST/Rust/src/solution_1.rs
+++ Minimum Absolute Difference in BST/Rust/src/solution_1.rs
@@ -1,20 +1,33 @@
 // https://leetcode.cn/problems/minimum-absolute-difference-in-bst/
 
 use std::cell::RefCell;
+use std::cmp::min;
 use std::rc::Rc;
+use rust::TreeNode;
 
 pub fn get_minimum_difference(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> i32 {
-    let mut result = vec![];
-    travel(root, &mut result);
-    result
+    let mut min_val = i32::MAX;
+    let mut pre_val = None;
+
+    travel(root, &mut min_val, &mut pre_val);
+
+    min_val
 }
 
-fn travel(root: Option<Rc<RefCell<TreeNode<i32>>>>, nums: &mut Vec<i32>) {
+fn travel(root: Option<Rc<RefCell<TreeNode<i32>>>>, min_val: &mut i32, pre_val: &mut Option<i32>) {
     if root.is_none() {
         return;
     }
 
-    travel(root.as_ref().unwrap().borrow_mut().left.take(), nums);
-    nums.push(root.as_ref().unwrap().borrow().val);
-    travel(root.as_ref().unwrap().borrow_mut().right.take(), nums);
+    travel(root.as_ref().unwrap().borrow_mut().left.take(), min_val, pre_val);
+
+    let val = root.as_ref().unwrap().borrow().val;
+    
+    if pre_val.is_some() {
+        *min_val = min(*min_val, (pre_val.unwrap() - val).abs());
+    }
+
+    *pre_val = Some(val);
+
+    travel(root.as_ref().unwrap().borrow_mut().right.take(), min_val, pre_val);
 }
