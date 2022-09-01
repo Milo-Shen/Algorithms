1d836ed2e2393c9f2a5078964b4d074774b8d6ba
Milo-Shen <Milo-Shen@outlook.com>
2022-09-01 22:03:32 +0800

    feat: update js code for combine

--- Combinations/Javascript/solution.js
+++ Combinations/Javascript/solution.js
@@ -3,13 +3,12 @@
 function combine(n, k) {
   let result = [];
 
-  let visited = Array(n).fill(false);
-  dfs(n, k, 0, 1, visited, [], result);
+  dfs(n, k, 0, 1, [], result);
 
   return result;
 }
 
-function dfs(n, k, depth, start_index, visited, subset, result) {
+function dfs(n, k, depth, start_index, subset, result) {
   if (depth === k) {
     result.push([...subset]);
     return;
@@ -17,7 +16,7 @@ function dfs(n, k, depth, start_index, visited, subset, result) {
 
   for (let i = start_index; i <= n; i++) {
     subset.push(i);
-    dfs(n, k, depth + 1, i + 1, visited, subset, result);
+    dfs(n, k, depth + 1, i + 1, subset, result);
     subset.pop();
   }
 }
---
+++ Combinations/Rust/src/solution.rs
@@ -0,0 +1,7 @@
+// https://leetcode.cn/problems/combinations/
+
+pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
+    let mut result: Vec<Vec<i32>> = Vec::new();
+
+    vec![]
+}
