70d15b6a7f6a22bf6f40468702cf65211ebb7083
MiloShen <Milo-Shen@outlook.com>
2022-09-04 17:16:39 +0800

    feat: add rust code for largestUniqueNumber

--- Largest Unique Number/Rust/src/solution_1.rs
+++ Largest Unique Number/Rust/src/solution_1.rs
@@ -1,5 +1,17 @@
 // https://leetcode.cn/problems/largest-unique-number/
 
 pub fn largest_unique_number(nums: Vec<i32>) -> i32 {
-    0
+    let mut count = [0; 1001];
+
+    for i in nums {
+        count[i as usize] = count[i as usize] + 1;
+    }
+
+    for i in (0..1001).rev() {
+        if count[i as usize] == 1 {
+            return i;
+        }
+    }
+
+    -1
 }
