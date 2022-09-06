e8014a8eaef4ee03d536d5a5073ed88142d4c5bf
Milo-Shen <Milo-Shen@outlook.com>
2022-09-06 21:21:11 +0800

    feat: update js solution for binary-search-tree-iterator

--- Binary Search Tree Iterator/Javascript/solution_1.js
+++ Binary Search Tree Iterator/Javascript/solution_1.js
@@ -19,7 +19,7 @@ BSTIterator.prototype.next = function () {
   // 如果当前节点没有右子树节点
   if (!node.right) {
     node = this.stack.pop();
-    // 一直 pop 知道寻找到一个左子树关系
+    // 一直 pop 直到寻找到一个左子树关系
     while (this.stack.length && this.stack[this.stack.length - 1].right === node) {
       node = this.stack.pop();
     }
@@ -41,5 +41,11 @@ BSTIterator.prototype.hasNext = function () {
 
 // test cases
 const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
-let nums = [1, 2, 4, 2, 3];
+let nums = [7, 3, 15, null, null, 9, 20];
 let binaryTree = buildBinaryTree(nums);
+let bst_iterator = new BSTIterator(binaryTree);
+console.log(bst_iterator.next());
+console.log(bst_iterator.next());
+console.log(bst_iterator.next());
+console.log(bst_iterator.next());
+console.log(bst_iterator.next());
