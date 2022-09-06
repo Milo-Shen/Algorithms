c3948caa10edb87c3d36a8c5d209e9efa75a15cd
Milo-Shen <Milo-Shen@outlook.com>
2022-09-06 21:27:40 +0800

    feat: init rust code for binary-search-tree-iterator

---
+++ Binary Search Tree Iterator/Rust/src/main.rs
@@ -0,0 +1,9 @@
+// https://leetcode-cn.com/problems/kTOapQ/submissions/
+// https://leetcode-cn.com/problems/binary-search-tree-iterator/submissions/
+// https://www.lintcode.com/problem/86/
+
+mod solution_1;
+
+fn main() {
+    println!("Hello, world!");
+}
