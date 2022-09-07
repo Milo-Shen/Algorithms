a2732517a7dec943f4e63570b201cd92ebd82634
Milo-Shen <Milo-Shen@outlook.com>
2022-09-07 22:06:40 +0800

    feat: add todo rust code for BSTIterator

--- Binary Search Tree Iterator/Rust/src/solution_1.rs
+++ Binary Search Tree Iterator/Rust/src/solution_1.rs
@@ -1,21 +1,45 @@
+use rust::TreeNode;
 use std::cell::RefCell;
 use std::rc::Rc;
-use rust::TreeNode;
 
 #[derive(Debug)]
-pub struct BSTIterator {}
+pub struct BSTIterator {
+    stack: Vec<Rc<RefCell<TreeNode<i32>>>>,
+}
 
 impl BSTIterator {
-
     pub fn new(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> Self {
-        Self{}
+        let mut stack = Vec::new();
+
+        // 特殊情况处理
+        if root.is_none() {
+            return Self { stack };
+        }
+
+        let mut cur_node = root;
+
+        while cur_node.is_some() {
+            if let Some(node) = cur_node {
+                cur_node = node.borrow_mut().left.take();
+                stack.push(node);
+            }
+        }
+
+        Self { stack }
     }
 
     pub fn next(&self) -> i32 {
+        if self.stack.is_empty() {
+            return i32::MIN;
+        }
+
+        let cur_node = &self.stack[self.stack.len() - 1];
+        // let cur_node = self.stack[self.stack.len() - 1];
+
         -1
     }
 
     pub fn has_next(&self) -> bool {
-        false
+        !self.stack.is_empty()
     }
 }
