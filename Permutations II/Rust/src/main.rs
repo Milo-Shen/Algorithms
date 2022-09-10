ea56521b989eebdd4a514ed2239ccfaeb7ab6e61
Milo-Shen <Milo-Shen@outlook.com>
2022-09-10 17:35:09 +0800

    feat: add rust solution 2 for Permutations

--- Permutations II/Rust/src/main.rs
+++ Permutations II/Rust/src/main.rs
@@ -2,9 +2,14 @@
 // https://leetcode.cn/problems/permutations-ii/
 
 mod solution_1;
+mod solution_2;
 
 fn main() {
     let nums = vec![1, 2, 3];
     let answer = solution_1::permute_unique(nums);
+    println!("answer = {:?}", answer);
+
+    let nums = vec![1, 2, 3];
+    let answer = solution_2::permute_unique(nums);
     println!("answer = {:?}", answer);
 }
---
+++ Permutations II/Rust/src/solution_2.rs
@@ -0,0 +1,49 @@
+// https://www.lintcode.com/problem/16/
+// https://leetcode.cn/problems/permutations-ii/
+
+pub fn permute_unique(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut result: Vec<Vec<i32>> = vec![];
+
+    // 异常检测
+    if nums.is_empty() {
+        return result;
+    }
+
+    // 对 nums 进行排序
+    let mut nums = nums;
+    nums.sort();
+
+    // 初始化 visited
+    let nums_len = nums.len();
+    let mut visited = Vec::with_capacity(nums_len);
+    for _ in 0..nums_len {
+        visited.push(false);
+    }
+
+    dfs(&nums, nums_len, &mut visited, 0, &mut result, &mut Vec::new());
+
+    result
+}
+
+fn dfs(nums: &Vec<i32>, nums_len: usize, visited: &mut Vec<bool>, start_index: usize, result: &mut Vec<Vec<i32>>, subset: &mut Vec<i32>) {
+    if start_index == nums_len {
+        result.push(subset.clone());
+        return;
+    }
+
+    for i in 0..nums_len {
+        if visited[i] {
+            continue;
+        }
+
+        if i > 0 && nums[i] == nums[i - 1] && !visited[i - 1] {
+            continue;
+        }
+
+        visited[i] = true;
+        subset.push(nums[i]);
+        dfs(nums, nums_len, visited, start_index + 1, result, subset);
+        visited[i] = false;
+        subset.pop();
+    }
+}
