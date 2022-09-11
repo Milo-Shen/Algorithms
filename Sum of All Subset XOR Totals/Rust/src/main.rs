498de9432b4c16f626614fe3ed1edddeda3e6404
MiloShen <Milo-Shen@outlook.com>
2022-09-11 15:14:26 +0800

    feat: add rust solution2 for sum-of-all-subset-xor-totals

--- Sum of All Subset XOR Totals/Rust/src/main.rs
+++ Sum of All Subset XOR Totals/Rust/src/main.rs
@@ -1,8 +1,12 @@
 // https://leetcode.cn/problems/sum-of-all-subset-xor-totals/
 
 mod solution_1;
+mod solution_2;
 
 fn main() {
     let answer = solution_1::subset_xor_sum(vec![1, 3]);
-    println!("answer = {}", answer);
+    println!("answer_1 = {}", answer);
+
+    let answer = solution_2::subset_xor_sum(vec![1, 3]);
+    println!("answer_2 = {}", answer);
 }
--- Sum of All Subset XOR Totals/Rust/src/solution_2.rs
+++ Sum of All Subset XOR Totals/Rust/src/solution_2.rs
@@ -15,22 +15,17 @@ pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
 }
 
 fn dfs(nums: &Vec<i32>, num_len: usize, start_index: usize, subsets: &mut Vec<i32>, sum: &mut i32) {
-    if start_index == num_len {
-        let mut temp = 0;
+    let mut temp = 0;
 
-        for value in subsets.iter() {
-            temp = temp ^ (*value);
-        }
-
-        *sum = *sum + temp;
-        return;
+    for value in subsets.iter() {
+        temp = temp ^ (*value);
     }
 
-    // 选 nums[index]
-    subsets.push(nums[start_index]);
-    dfs(nums, num_len, start_index + 1, subsets, sum);
+    *sum = *sum + temp;
 
-    // 不选 nums[index]
-    subsets.pop();
-    dfs(nums, num_len, start_index + 1, subsets, sum);
+    for i in start_index..num_len {
+        subsets.push(nums[i]);
+        dfs(nums, num_len, i + 1, subsets, sum);
+        subsets.pop();
+    }
 }
