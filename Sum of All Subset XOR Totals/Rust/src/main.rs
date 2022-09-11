24cca89aa662233dc9255ec7890985a26de1f298
MiloShen <Milo-Shen@outlook.com>
2022-09-11 14:14:04 +0800

    feat: init rust code for subset_xor_sum

---
+++ Sum of All Subset XOR Totals/Rust/src/main.rs
@@ -0,0 +1,8 @@
+// https://leetcode.cn/problems/sum-of-all-subset-xor-totals/
+
+mod solution_1;
+
+fn main() {
+    let answer = solution_1::subset_xor_sum(vec![1, 3]);
+    println!("answer = {}", answer);
+}
---
+++ Sum of All Subset XOR Totals/Rust/src/solution_1.rs
@@ -0,0 +1,5 @@
+// https://leetcode.cn/problems/sum-of-all-subset-xor-totals/
+
+pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
+    -1
+}
