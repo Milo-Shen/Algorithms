b7c9c50994fdc94a1147b1d48f6a95f98de7a0dc
MiloShen <Milo-Shen@outlook.com>
2022-09-11 20:31:31 +0800

    feat: add js solution 1 for symmetric-tree

--- Symmetric Tree/Javascript/solution_1.js
+++ Symmetric Tree/Javascript/solution_1.js
@@ -1,1 +1,30 @@
 // https://leetcode.cn/problems/symmetric-tree/
+
+function isSymmetric(root) {
+  if (!root) {
+    return true;
+  }
+  return divideConquer(root.left, root.right);
+}
+
+function divideConquer(root1, root2) {
+  if (!root1 && !root2) {
+    return true;
+  }
+
+  if (!root1 || !root2) {
+    return false;
+  }
+
+  return (
+    root1.val === root2.val &&
+    divideConquer(root1.left, root2.right) &&
+    divideConquer(root1.right, root2.left)
+  );
+}
+
+// test cases
+const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
+let nums = [1, 2, 2, 3, 4, 4, 3];
+let root = buildBinaryTree(nums);
+console.log(isSymmetric(root));
