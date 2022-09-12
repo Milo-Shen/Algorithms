5907f8164c71c1c7402b3f315b48191ff7c9f2d2
Milo-Shen <Milo-Shen@outlook.com>
2022-09-12 12:49:55 +0800

    feat: add rust solution for minimum-depth-of-binary-tree

--- Minimum Depth of Binary Tree/Rust/src/main.rs
+++ Minimum Depth of Binary Tree/Rust/src/main.rs
@@ -1,7 +1,12 @@
 // https://leetcode.cn/problems/minimum-depth-of-binary-tree/submissions/
 
+use binary_tree::build_binary_tree;
+
 mod solution_1;
 
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![Some(3), Some(9), Some(20), None, None, Some(15), Some(7)];
+    let root_node = build_binary_tree(nums);
+    let answer = solution_1::min_depth(root_node);
+    println!("answer = {:?}", answer);
 }
--- Minimum Depth of Binary Tree/Rust/src/solution_1.rs
+++ Minimum Depth of Binary Tree/Rust/src/solution_1.rs
@@ -1,7 +1,25 @@
+// https://leetcode.cn/problems/minimum-depth-of-binary-tree/
+
 use binary_tree::TreeNode;
 use std::cell::RefCell;
+use std::cmp::min;
 use std::rc::Rc;
 
 pub fn min_depth(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> i32 {
-    -1
+    if root.is_none() {
+        return 0;
+    }
+
+    if root.as_ref().unwrap().borrow().left.is_none() {
+        return min_depth(root.as_ref().unwrap().borrow_mut().right.take()) + 1;
+    }
+
+    if root.as_ref().unwrap().borrow().right.is_none() {
+        return min_depth(root.as_ref().unwrap().borrow_mut().left.take()) + 1;
+    }
+
+    let min_left = min_depth(root.as_ref().unwrap().borrow_mut().left.take());
+    let min_right = min_depth(root.as_ref().unwrap().borrow_mut().right.take());
+
+    min(min_left, min_right) + 1
 }
