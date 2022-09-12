e4ab6ea6eb66e6ccb9a8fe98495b9afb663e397d
Milo-Shen <Milo-Shen@outlook.com>
2022-09-12 15:28:03 +0800

    feat: add js solution for pascals-triangle

--- Pascal's Triangle/Javascript/solution_1.js
+++ Pascal's Triangle/Javascript/solution_1.js
@@ -1,5 +1,23 @@
 // https://leetcode.cn/problems/pascals-triangle/
 
-function generate(numRows) {}
+function generate(numRows) {
+  let result = [];
+  // 先构建数组
+
+  for (let i = 1; i <= numRows; i++) {
+    let arr = Array(i).fill(0);
+    arr[0] = arr[i - 1] = 1;
+
+    if (i >= 3) {
+      for (let j = 1; j < i - 1; j++) {
+        arr[j] = result[i - 2][j - 1] + result[i - 2][j];
+      }
+    }
+
+    result.push(arr);
+  }
+
+  return result;
+}
 
 console.log(generate(5));
