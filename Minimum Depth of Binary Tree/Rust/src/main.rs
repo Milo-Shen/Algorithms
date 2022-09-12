d2ed782b92d950985158438d56707ad05610bc03
Milo-Shen <Milo-Shen@outlook.com>
2022-09-12 12:32:13 +0800

    feat: init rust code for min_depth

---
+++ Minimum Depth of Binary Tree/Rust/src/main.rs
@@ -0,0 +1,7 @@
+// https://leetcode.cn/problems/minimum-depth-of-binary-tree/submissions/
+
+mod solution_1;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ Minimum Depth of Binary Tree/Rust/src/solution_1.rs
@@ -0,0 +1,7 @@
+use binary_tree::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub fn min_depth(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> i32 {
+    -1
+}
