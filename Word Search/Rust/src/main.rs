811062e443207b394e360b0ef1078e38fafd9a77
Milo-Shen <Milo-Shen@outlook.com>
2022-09-18 14:46:42 +0800

    feat: init rust code for Word Search

---
+++ Word Search/Rust/src/main.rs
@@ -0,0 +1,16 @@
+// https://www.lintcode.com/problem/132
+// https://leetcode.cn/problems/word-search/
+
+mod solution_1;
+
+fn main() {
+    let board = vec![
+        vec!['A', 'B', 'C', 'E'],
+        vec!['S', 'F', 'C', 'S'],
+        vec!['A', 'D', 'E', 'E'],
+    ];
+
+    let word = String::from("ACCEDED");
+    let answer = solution_1::exist(board, word);
+    println!("answer = {}", answer);
+}
---
+++ Word Search/Rust/src/solution_1.rs
@@ -0,0 +1,3 @@
+pub fn exist(board: Vec<Vec<char>>, word: String) -> bool {
+    false
+}
