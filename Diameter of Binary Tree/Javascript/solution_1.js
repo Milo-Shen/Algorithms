905f2414622557458d411bdff0cdbfb0b0f33ff7
Milo-Shen <Milo-Shen@outlook.com>
2022-09-21 21:33:45 +0800

    feat: update js code for diameter-of-binary-tree

--- Diameter of Binary Tree/Javascript/solution_1.js
+++ Diameter of Binary Tree/Javascript/solution_1.js
@@ -1,21 +1,29 @@
 // https://leetcode.cn/problems/diameter-of-binary-tree/
 
 function diameterOfBinaryTree(root) {
-  let max = -Infinity;
+  let max = { val: -Infinity };
 
-  const count = (root) => {
-    if (root === null) {
-      return 0;
-    }
-    let left = count(root.left);
-    let right = count(root.right);
+  depth(root, max);
 
-    // 经过的结点数 = 某个结点左子树深度 + 右子树深度 + 1 (此节点本身)
-    // 当前经过的结点数最大值 = Math.max(当前经过的结点数, 上一轮经过的结点数最大值)
-    max = Math.max(left + right + 1, max);
-    return Math.max(left, right) + 1;
-  };
-
-  count(root);
-  return max - 1; // 直径长度 = 经过的结点数最大值 - 1
+  // 直径长度 = 经过的结点数最大值 - 1
+  return max.val - 1;
 }
+
+const depth = (root, max) => {
+  if (root === null) {
+    return 0;
+  }
+  let left = depth(root.left, max);
+  let right = depth(root.right, max);
+
+  // 经过的结点数 = 某个结点左子树深度 + 右子树深度 + 1 (此节点本身)
+  // 当前经过的结点数最大值 = Math.max(当前经过的结点数, 上一轮经过的结点数最大值)
+  max.val = Math.max(left + right + 1, max.val);
+  return Math.max(left, right) + 1;
+};
+
+// test cases
+const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
+let nums = [3, 9, 20, null, null, 15, 7];
+let binaryTree = buildBinaryTree(nums);
+console.log(diameterOfBinaryTree(binaryTree));
