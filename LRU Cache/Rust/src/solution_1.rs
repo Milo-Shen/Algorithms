0da24de41f95bf873ad4d5b8b29e487e1ba44f1d
Milo-Shen <Milo-Shen@outlook.com>
2022-09-25 21:56:55 +0800

    feat: update rust solution_1 for LRUCache

--- LRU Cache/Rust/src/solution_1.rs
+++ LRU Cache/Rust/src/solution_1.rs
@@ -3,36 +3,68 @@
 // https://leetcode.cn/problems/lru-cache-lcci/
 // https://leetcode.cn/problems/OrIXps/
 
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
+
 struct LinkedNode {
     key: i32,
     val: i32,
     next: Option<Box<LinkedNode>>,
 }
 
-pub struct LRUCache<'a> {
+impl LinkedNode {
+    fn new(key: i32, val: i32, next: Option<Box<LinkedNode>>) -> Self {
+        Self {
+            key,
+            val,
+            next: None,
+        }
+    }
+}
+
+pub struct LRUCache {
     // Cache 的最大容量, 超过容量就需要淘汰数据
     capacity: i32,
     // 单链表 dummy 头
-    dummy: LinkedNode,
+    dummy: Rc<RefCell<LinkedNode>>,
     // 单链表的尾节点
-    tail: &'a Option<Box<LinkedNode>>,
+    tail: Rc<RefCell<LinkedNode>>,
+    // 存储映射关系: key => key 所对应数据节点之前的节点
+    key_to_prev: HashMap<i32, Rc<RefCell<LinkedNode>>>,
 }
 
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
-impl<'a> LRUCache<'a> {
+impl LRUCache {
+    fn kick(&self, key: i32) {}
+
     pub fn new(capacity: i32) -> Self {
+        let node = LinkedNode::new(-1, -1, None);
+        let dummy = Rc::new(RefCell::new(node));
+        let tail = Rc::clone(&dummy);
+
         LRUCache {
             capacity,
-            dummy: todo!(),
-            tail: todo!(),
+            dummy,
+            tail,
+            key_to_prev: HashMap::new(),
         }
     }
 
     pub fn get(&self, key: i32) -> i32 {
-        -1
+        // 如果这个 key 根本不存在于缓存, 返回 -1
+        if self.key_to_prev.contains_key(&key) {
+            return -1;
+        }
+
+        // 这个 key 刚刚被访问过, 因此该节点应当被移动到链表尾部
+        self.kick(key);
+
+        // 这个节点被移动到了链表尾部, 成为了 tail 节点, 返回 tail 的值
+        self.tail.borrow().val
     }
 
     pub fn put(&self, key: i32, value: i32) {}
