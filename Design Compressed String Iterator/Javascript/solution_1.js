743d23e930c7d270705edb7b6320ad5e89fcc3b5
Milo-Shen <Milo-Shen@outlook.com>
2022-09-25 22:57:45 +0800

    feat: add js code for Design Compressed String Iterator

--- Design Compressed String Iterator/Javascript/solution_1.js
+++ Design Compressed String Iterator/Javascript/solution_1.js
@@ -3,17 +3,49 @@
 /**
  * @param {string} compressedString
  */
-let StringIterator = function (compressedString) {};
+let StringIterator = function (compressedString) {
+  this.array = [];
+
+  let num = '';
+  let char = null;
+
+  for (let i = 0, len = compressedString.length; i < len; i++) {
+    if (Number.isNaN(Number(compressedString[i]))) {
+      if (char) {
+        for (let j = 0; j < ~~num; j++) {
+          this.array.push(char);
+        }
+      }
+
+      char = compressedString[i];
+      num = '';
+    } else {
+      num += compressedString[i];
+    }
+  }
+
+  for (let i = 0; i < ~~num; i++) {
+    this.array.push(char);
+  }
+};
 
 /**
- * @return {character}
+ * @returns {*}
  */
-StringIterator.prototype.next = function () {};
+StringIterator.prototype.next = function () {
+  if (!this.hasNext()) {
+    return ' ';
+  }
+
+  return this.array.shift();
+};
 
 /**
  * @return {boolean}
  */
-StringIterator.prototype.hasNext = function () {};
+StringIterator.prototype.hasNext = function () {
+  return !!this.array.length;
+};
 
 /**
  * Your StringIterator object will be instantiated and called as such:
@@ -21,7 +53,8 @@ StringIterator.prototype.hasNext = function () {};
  * var param_1 = obj.next()
  * var param_2 = obj.hasNext()
  */
-let obj = new StringIterator('L1e2t1C1o1d1e1');
+let obj = new StringIterator('L10e2t1C1o1d1e11');
+console.log(obj);
 let param_1 = obj.next();
 let param_2 = obj.hasNext();
 console.log(param_1, param_2);
