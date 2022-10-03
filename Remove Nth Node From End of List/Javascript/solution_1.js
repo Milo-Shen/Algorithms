62cbb48b9606977cbdabb9044a8e3047f1bb0650
Milo-Shen <Milo-Shen@outlook.com>
2022-10-03 22:05:19 +0800

    feat: init js solution 1 for remove-nth-node-from-end-of-list

---
+++ Remove Nth Node From End of List/Javascript/solution_1.js
@@ -0,0 +1,9 @@
+// https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
+
+const { build_list, print_list } = require('../../Base/List/Javascript/List');
+
+function removeNthFromEnd(head, n) {}
+
+let list = build_list([1, 2, 3, 4, 5]);
+removeNthFromEnd(list, 2);
+print_list(list);
