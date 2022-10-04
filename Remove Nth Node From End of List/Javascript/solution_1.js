8bcb081409cf598b8074073366ed7ba0343f9020
MiloShen <Milo-Shen@outlook.com>
2022-10-04 11:10:38 +0800

    feat: add js solution 1 for remove-nth-node-from-end-of-list

--- Remove Nth Node From End of List/Javascript/solution_1.js
+++ Remove Nth Node From End of List/Javascript/solution_1.js
@@ -2,8 +2,30 @@
 
 const { build_list, print_list } = require('../../Base/List/Javascript/List');
 
-function removeNthFromEnd(head, n) {}
+function removeNthFromEnd(head, n) {
+  let list = [];
 
-let list = build_list([1, 2, 3, 4, 5]);
-removeNthFromEnd(list, 2);
-print_list(list);
+  let node = head;
+
+  while (node) {
+    list.push(node);
+    node = node.next;
+  }
+
+  let max = list.length;
+  let pre = max - n - 1;
+  let cur = max - n;
+
+  if (cur === 0) {
+    head = head.next;
+    return head;
+  }
+
+  list[pre].next = list[cur].next;
+
+  return head;
+}
+
+let list = build_list([1, 2]);
+let header = removeNthFromEnd(list, 2);
+print_list(header);
