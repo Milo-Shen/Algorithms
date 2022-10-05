e6ae25910061ff4ad33f2e349b4c462c1606b872
MiloShen <Milo-Shen@outlook.com>
2022-10-05 16:22:59 +0800

    feat: init Ugly Number II

---
+++ Ugly Number II/Rust/src/main.rs
@@ -0,0 +1,8 @@
+// https://leetcode.cn/problems/ugly-number-ii/
+
+mod solution_1;
+
+fn main() {
+    let answer = solution_1::nth_ugly_number(6);
+    println!("answer = {}", answer);
+}
---
+++ Ugly Number II/Rust/src/solution_1.rs
@@ -0,0 +1,5 @@
+// https://leetcode.cn/problems/ugly-number-ii/
+
+pub fn nth_ugly_number(n: i32) -> i32 {
+    1
+}
