254b8036833709eab9c584419abae1870c6aa360
Milo-Shen <Milo-Shen@outlook.com>
2022-10-07 17:28:25 +0800

    feat: add js solution for roman-to-integer

---
+++ Roman to Integer/Javascript/solution_1.js
@@ -0,0 +1,28 @@
+// https://leetcode.cn/problems/roman-to-integer/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const romanToInt = function (s) {
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let res = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] < map[s[i + 1]]) {
+      res -= map[s[i]];
+    } else {
+      res += map[s[i]];
+    }
+  }
+  return res;
+};
+
+console.log(romanToInt('III'));
