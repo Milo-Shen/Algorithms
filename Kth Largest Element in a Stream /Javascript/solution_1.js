aeab4f00fb8fc155bdb28f7580f6112375ed1c4f
Milo-Shen <Milo-Shen@outlook.com>
2022-10-08 22:09:20 +0800

    feat: init Kth Largest Element in a Stream

---
+++ Kth Largest Element in a Stream /Javascript/solution_1.js
@@ -0,0 +1,19 @@
+// https://leetcode.cn/problems/kth-largest-element-in-a-stream/
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+const KthLargest = function (k, nums) {};
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function (val) {};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
