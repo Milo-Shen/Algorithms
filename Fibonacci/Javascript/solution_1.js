ad8a2d7caadcc54632832c56da7592bc425b2ae0
MiloShen <Milo-Shen@outlook.com>
2022-10-09 14:35:23 +0800

    feat: update js code for Fibonacci

--- Fibonacci/Javascript/solution_1.js
+++ Fibonacci/Javascript/solution_1.js
@@ -1,10 +1,11 @@
-// Recursion
+// https://leetcode.cn/problems/fibonacci-number/
 
+// Recursion
 // 1. 递归的定义: 函数接受什么样的参数，返回什么样的值，代表什么样的意思
 function fibonacci(n) {
   // 3. 递归的出口
-  if (n <= 2) {
-    return n - 1;
+  if (n < 2) {
+    return n;
   }
 
   // 2. 递归的拆解
--- Fibonacci/Javascript/solution_2.js
+++ Fibonacci/Javascript/solution_2.js
@@ -1,19 +1,19 @@
-// Recursion with memory
+// https://leetcode.cn/problems/fibonacci-number/
 
+// Recursion with memory
 // 通过 hash 进行剪枝操作
 let hash = {};
 
 // 1. 递归的定义: 函数接受什么样的参数，返回什么样的值，代表什么样的意思
 function fibonacci(n) {
-
   // 进行减枝操作
   if (hash[n]) {
     return hash[n];
   }
 
   // 3. 递归的出口
-  if (n <= 2) {
-    return n - 1;
+  if (n < 2) {
+    return n;
   }
 
   // 2. 递归的拆解
