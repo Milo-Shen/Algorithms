9bf54dffc2d22175f5ce86485bfb84d8b1f87d16
Milo-Shen <Milo-Shen@outlook.com>
2022-10-10 09:20:49 +0800

    feat: init word break

---
+++ Word Break /Javascript/solution_1.js
@@ -0,0 +1,6 @@
+// https://leetcode.cn/problems/word-break/
+// https://www.lintcode.com/problem/107/
+
+const wordBreak = function (s, wordDict) {};
+
+console.log(wordBreak('leetcode', ['leet', 'code']));
