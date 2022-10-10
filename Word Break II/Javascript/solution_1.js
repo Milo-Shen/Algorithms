f32c6036210cfefffd5fd53e39e7eec49a64dd2d
Milo-Shen <Milo-Shen@outlook.com>
2022-10-10 16:27:40 +0800

    feat: init word break 2

---
+++ Word Break II/Javascript/solution_1.js
@@ -0,0 +1,6 @@
+// https://leetcode.cn/problems/word-break-ii/
+// https://www.lintcode.com/problem/582/
+
+const wordBreak = function (s, wordDict) {};
+
+console.log(wordBreak('catsanddog', ['cat', 'cats', 'and', 'sand', 'dog']));
