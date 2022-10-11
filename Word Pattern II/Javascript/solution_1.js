077e3d7bfc27af9945523313fbf18f2b0e6c0b47
Milo-Shen <Milo-Shen@outlook.com>
2022-10-11 11:32:37 +0800

    feat: init Word Pattern II

---
+++ Word Pattern II/Javascript/solution_1.js
@@ -0,0 +1,6 @@
+// https://leetcode.cn/problems/word-pattern-ii/
+// https://www.lintcode.com/problem/829
+
+const wordPatternMatch = function (pattern, s) {};
+
+console.log(wordPatternMatch('abab', 'redblueredblue'));
