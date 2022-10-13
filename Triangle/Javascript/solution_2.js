85202b0d166aa626fe79554ae59be41251c855ab
Milo-Shen <Milo-Shen@outlook.com>
2022-10-13 21:32:12 +0800

    feat: add solution 2 for minimumTotal

---
+++ Triangle/Javascript/solution_2.js
@@ -0,0 +1,19 @@
+// https://leetcode.cn/problems/IlPe0q/
+// https://www.lintcode.com/problem/109/
+
+const minimumTotal = function (triangle) {
+  return divide_conquer(triangle, 0, 0);
+};
+
+function divide_conquer(triangle, x, y) {
+  if (x === triangle.length) {
+    return 0;
+  }
+
+  let left = divide_conquer(triangle, x + 1, y);
+  let right = divide_conquer(triangle, x + 1, y + 1);
+  return Math.min(left, right) + triangle[x][y];
+}
+
+let triangle = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]];
+console.log(minimumTotal(triangle));
