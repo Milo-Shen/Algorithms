69959b591ad4ce412ab533544e0aaf73ecb1efd0
Milo-Shen <Milo-Shen@outlook.com>
2022-10-15 15:05:16 +0800

    feat: add rust solution for can_win_bash

---
+++ Bash Game/Rust/src/main.rs
@@ -0,0 +1,8 @@
+// https://www.lintcode.com/problem/1300/solution
+
+mod solution_1;
+
+fn main() {
+    let answer = solution_1::can_win_bash(4);
+    println!("answer = {}", answer);
+}
---
+++ Bash Game/Rust/src/solution_1.rs
@@ -0,0 +1,3 @@
+pub fn can_win_bash(n: i32) -> bool {
+    n % 4 != 0
+}
