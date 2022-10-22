b959b779bc27a2025ebb7958850104ee2e25bc54
Milo-Shen <Milo-Shen@outlook.com>
2022-10-22 15:24:52 +0800

    feat: add rust solution 1 for check-if-array-is-sorted-and-rotated

--- Check if Array Is Sorted and Rotated/Rust/src/main.rs
+++ Check if Array Is Sorted and Rotated/Rust/src/main.rs
@@ -1,3 +1,9 @@
+// https://leetcode.cn/problems/check-if-array-is-sorted-and-rotated/
+
+mod solution_1;
+
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![3, 4, 5, 1, 2];
+    let answer = solution_1::check(nums);
+    println!("answer = {}", answer);
 }
--- Check if Array Is Sorted and Rotated/Rust/src/solution_1.rs
+++ Check if Array Is Sorted and Rotated/Rust/src/solution_1.rs
@@ -1,2 +1,14 @@
 // https://leetcode.cn/problems/check-if-array-is-sorted-and-rotated/
 
+pub fn check(nums: Vec<i32>) -> bool {
+    let mut count = 0;
+    let len = nums.len();
+
+    for i in 0..len {
+        if nums[i] > nums[ ( i + 1 ) % len ] {
+            count = count + 1;
+        }
+    }
+
+    count <= 1
+}
