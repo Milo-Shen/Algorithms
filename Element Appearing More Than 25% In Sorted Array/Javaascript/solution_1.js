703f6a83cae251d7bff167d5b0eba45d83bb437e
Milo-Shen <Milo-Shen@outlook.com>
2022-10-25 16:21:51 +0800

    feat: init element-appearing-more-than-25-in-sorted-array

---
+++ Element Appearing More Than 25% In Sorted Array/Javaascript/solution_1.js
@@ -0,0 +1,5 @@
+// https://leetcode.cn/problems/element-appearing-more-than-25-in-sorted-array/
+
+const findSpecialInteger = function (arr) {};
+
+console.log(findSpecialInteger([1, 2, 2, 6, 6, 6, 6, 7, 10]));
