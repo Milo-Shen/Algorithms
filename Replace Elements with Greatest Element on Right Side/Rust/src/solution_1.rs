32a525c163e0316c3bf9585ce98ce5a5c837399a
Milo-Shen <Milo-Shen@outlook.com>
2022-11-13 21:28:00 +0800

    feat: add rust solution 1 for replace-elements-with-greatest-element-on-right-side

--- Replace Elements with Greatest Element on Right Side/Rust/src/solution_1.rs
+++ Replace Elements with Greatest Element on Right Side/Rust/src/solution_1.rs
@@ -1,5 +1,14 @@
 // https://leetcode.cn/problems/replace-elements-with-greatest-element-on-right-side/
 
 pub fn replace_elements(arr: Vec<i32>) -> Vec<i32> {
-    Vec::new()
+    let mut max = -1;
+    let mut arr = arr;
+
+    for i in (0..arr.len()).rev() {
+        let mut num = arr[i];
+        arr[i] = max;
+        max = if num > max { num } else { num }
+    }
+
+    arr
 }
