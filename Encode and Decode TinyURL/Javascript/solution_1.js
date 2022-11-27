b813bf7c748d17eaf66f634fb4762187854c0b06
Milo-Shen <Milo-Shen@outlook.com>
2022-11-27 21:06:53 +0800

    feat: add js solution 1 for encode-and-decode-tinyurl

---
+++ Encode and Decode TinyURL/Javascript/solution_1.js
@@ -0,0 +1,21 @@
+// https://leetcode.cn/problems/encode-and-decode-tinyurl/
+
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+const encode = function (longUrl) {
+    return longUrl;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+const decode = function (shortUrl) {
+    return shortUrl;
+};
