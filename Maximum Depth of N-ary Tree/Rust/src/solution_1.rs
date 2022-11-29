6e69fd7ce902a045b825ada6cd05f6ecb5ac36fd
Milo-Shen <Milo-Shen@outlook.com>
2022-11-29 21:48:59 +0800

    feat: add rust solution 1 for max-increase-to-keep-city-skyline

--- Maximum Depth of N-ary Tree/Rust/src/solution_1.rs
+++ Maximum Depth of N-ary Tree/Rust/src/solution_1.rs
@@ -1,3 +1,22 @@
 pub fn max_increase_keeping_skyline(grid: Vec<Vec<i32>>) -> i32 {
-    0
+    let n = grid.len();
+    let mut row = vec![0; n];
+    let mut col = vec![0; n];
+
+    for i in 0..n {
+        for j in 0..n {
+            row[i] = row[i].max(grid[i][j]);
+            col[j] = col[j].max(grid[i][j]);
+        }
+    }
+
+    let mut ans = 0;
+
+    for i in 0..n {
+        for j in 0..n {
+            ans += row[i].min(col[j]) - grid[i][j];
+        }
+    }
+
+    ans
 }
