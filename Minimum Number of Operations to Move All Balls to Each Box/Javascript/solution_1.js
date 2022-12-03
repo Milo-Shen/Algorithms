4618e98af63e226eb5e5ec9be354ffc10dc8773d
Milo-Shen <Milo-Shen@outlook.com>
2022-12-03 20:52:54 +0800

    feat: add js solution 1 for minimum-number-of-operations-to-move-all-balls-to-each-box

--- Minimum Number of Operations to Move All Balls to Each Box/Javascript/solution_1.js
+++ Minimum Number of Operations to Move All Balls to Each Box/Javascript/solution_1.js
@@ -1,3 +1,22 @@
 // https://leetcode.cn/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
 
-const minOperations = function (boxes) {};
+const minOperations = function (boxes) {
+  const len = boxes.length;
+  const result = [];
+
+  for (let i = 0; i < len; i++) {
+    let sum = 0;
+
+    for (let j = 0; j < len; j++) {
+      if (boxes[j] === '1') {
+        sum += Math.abs(j - i);
+      }
+    }
+
+    result.push(sum);
+  }
+
+  return result;
+};
+
+console.log(minOperations('110'));
