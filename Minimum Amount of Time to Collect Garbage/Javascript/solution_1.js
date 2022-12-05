d69d4c11a1028b8bdece1285e83208b1e0a7e593
Milo-Shen <Milo-Shen@outlook.com>
2022-12-05 20:59:26 +0800

    feat: add js solution 1 for minimum-amount-of-time-to-collect-garbage

--- Minimum Amount of Time to Collect Garbage/Javascript/solution_1.js
+++ Minimum Amount of Time to Collect Garbage/Javascript/solution_1.js
@@ -1,2 +1,40 @@
 // https://leetcode.cn/problems/minimum-amount-of-time-to-collect-garbage/
 
+const garbageCollection = function (garbage, travel) {
+  let m_to = -1;
+  let p_to = -1;
+  let g_to = -1;
+  let time = 0;
+
+  for (let i = garbage.length - 1; i >= 0; i--) {
+    if (m_to === -1 && garbage[i].includes('M')) {
+      m_to = i;
+    }
+    if (p_to === -1 && garbage[i].includes('P')) {
+      p_to = i;
+    }
+    if (g_to === -1 && garbage[i].includes('G')) {
+      g_to = i;
+    }
+
+    time += garbage[i].length;
+  }
+
+  for (let i = 0; i < travel.length; i++) {
+    if (i < m_to) {
+      time += travel[i];
+    }
+    if (i < p_to) {
+      time += travel[i];
+    }
+    if (i < g_to) {
+      time += travel[i];
+    }
+  }
+
+  return time;
+};
+
+const garbage = ['G', 'P', 'GP', 'GG'];
+const travel = [2, 4, 3];
+console.log(garbageCollection(garbage, travel));
