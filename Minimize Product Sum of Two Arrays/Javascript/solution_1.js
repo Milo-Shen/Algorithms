dd0b2515dd37f15f0237540eec89107df9fae990
Milo-Shen <Milo-Shen@outlook.com>
2022-12-08 21:36:00 +0800

    feat: add js solution 1 for minimize-product-sum-of-two-arrays

---
+++ Minimize Product Sum of Two Arrays/Javascript/solution_1.js
@@ -0,0 +1,19 @@
+// https://leetcode.cn/problems/minimize-product-sum-of-two-arrays/
+
+const minProductSum = function (nums1, nums2) {
+  nums1 = nums1.sort();
+  nums2 = nums2.sort();
+
+  let answer = 0;
+  let length = nums1.length;
+
+  for (let i = 0; i < length; i++) {
+    answer += nums1[i] * nums2[length - 1 - i];
+  }
+
+  return answer;
+};
+
+let nums1 = [5, 3, 4, 2];
+let nums2 = [4, 2, 2, 5];
+console.log(minProductSum(nums1, nums2));
