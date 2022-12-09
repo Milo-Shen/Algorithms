09df8e9c675e56b2904a598f45d00510795ebfe1
Milo-Shen <Milo-Shen@outlook.com>
2022-12-09 21:59:06 +0800

    feat: init rust solution for minimize-product-sum-of-two-arrays

---
+++ Check if the Sentence Is Pangram/Rust/src/main.rs
@@ -0,0 +1,8 @@
+// https://leetcode.cn/problems/minimize-product-sum-of-two-arrays/
+
+mod solution_1;
+
+fn main() {
+    let nums1 = vec![5, 3, 4, 2];
+    let nums2 = vec![4, 2, 2, 5];
+}
---
+++ Check if the Sentence Is Pangram/Rust/src/solution_1.rs
@@ -0,0 +1,5 @@
+// https://leetcode.cn/problems/minimize-product-sum-of-two-arrays/submissions/
+
+pub fn min_product_sum(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+    0
+}
