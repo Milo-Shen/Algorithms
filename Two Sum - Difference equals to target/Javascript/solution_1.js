886bbcce6fccfb3d2f6c3817ff4b7638cc93e4f2
Milo-Shen <Milo-Shen@outlook.com>
2022-12-15 21:46:36 +0800

    feat: add js solution 1 for twoSum7

--- Two Sum - Difference equals to target/Javascript/solution_1.js
+++ Two Sum - Difference equals to target/Javascript/solution_1.js
@@ -1,5 +1,20 @@
 // https://www.lintcode.com/problem/610/
 
 function twoSum7(nums, target) {
-  // write your code here
+  let len = nums.length;
+  let result = [];
+
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (nums[j] - nums[i] === Math.abs(target)) {
+        result = [nums[i], nums[j]];
+      }
+    }
+  }
+
+  return result;
 }
+
+let nums = [0, 5, 7];
+let target = -2;
+console.log(twoSum7(nums, target));
