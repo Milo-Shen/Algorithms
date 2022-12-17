7c71a5db627936105cf5c0025eebcf7e5c041767
Milo-Shen <Milo-Shen@outlook.com>
2022-12-17 20:56:27 +0800

    feat: add rust solution 2 for twoSum7

--- Two Sum - Difference equals to target/Rust/src/main.rs
+++ Two Sum - Difference equals to target/Rust/src/main.rs
@@ -1,10 +1,16 @@
 // https://www.lintcode.com/problem/610/
 
 mod solution_1;
+mod solution_2;
 
 fn main() {
     let nums = vec![0, 5, 7];
     let target = -2;
     let answer = solution_1::two_sum7(nums, target);
+    println!("answer = {:?}", answer);
+
+    let nums = vec![0, 5, 7];
+    let target = -2;
+    let answer = solution_2::two_sum7(nums, target);
     println!("answer = {:?}", answer);
 }
