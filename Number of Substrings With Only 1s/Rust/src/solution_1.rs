fa4ddf75e24e4e39c965f4650ce97a0a93d6a8b4
Milo-Shen <Milo-Shen@outlook.com>
2022-12-18 20:44:23 +0800

    feat: add rust solution 1 for number-of-subsings-with-only-1s

--- Number of Substrings With Only 1s/Rust/src/solution_1.rs
+++ Number of Substrings With Only 1s/Rust/src/solution_1.rs
@@ -1,5 +1,28 @@
 // https://leetcode.cn/problems/number-of-subsings-with-only-1s/
 
+use std::cmp::max;
+
 pub fn num_sub(s: String) -> i32 {
-    0
+    let char_arr = s.chars().collect::<Vec<char>>();
+    let str_len = char_arr.len();
+    let MOD = 1000000007;
+
+    let mut j = 1;
+    let mut answer = 0;
+
+    for i in 0..str_len {
+        if char_arr[i] != '1' {
+            continue;
+        }
+
+        j = max(j, i + 1);
+        while j < str_len && char_arr[j] == '1' {
+            j += 1;
+        }
+
+        answer += j - i;
+        answer = answer % MOD;
+    }
+
+    answer as i32
 }
