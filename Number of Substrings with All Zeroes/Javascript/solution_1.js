3d86c6261ff9765e61ebc615fda0e4c233351f88
Milo-Shen <Milo-Shen@outlook.com>
2022-12-18 17:28:53 +0800

    feat: add js solution 1 for Number of Substrings with All Zeroes

--- Number of Substrings with All Zeroes/Javascript/solution_1.js
+++ Number of Substrings with All Zeroes/Javascript/solution_1.js
@@ -1,7 +1,25 @@
 // https://www.lintcode.com/problem/1870/
 
 function stringCount(str) {
-  return 0;
+  let str_len = str.length;
+
+  let j = 1;
+  let answer = 0;
+
+  for (let i = 0; i < str_len; i++) {
+    if (str[i] !== '0') {
+      continue;
+    }
+
+    j = Math.max(j, i + 1);
+    while (j < str_len && str[j] === '0') {
+      j++;
+    }
+
+    answer += j - i;
+  }
+
+  return answer;
 }
 
 console.log(stringCount('00010011'));
