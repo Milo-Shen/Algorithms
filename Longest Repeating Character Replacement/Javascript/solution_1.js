3d7ed7beddef77806aea81bc0bf158f553b1ebd2
Milo-Shen <Milo-Shen@outlook.com>
2022-12-22 19:49:57 +0800

    feat: update Longest Repeating Character Replacement

--- Longest Repeating Character Replacement/Javascript/solution_1.js
+++ Longest Repeating Character Replacement/Javascript/solution_1.js
@@ -1,4 +1,5 @@
 // https://www.lintcode.com/problem/1246/
+// https://leetcode.cn/problems/longest-repeating-character-replacement/
 
 function characterReplacement(s, k) {
   // write your code here
