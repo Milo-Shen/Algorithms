b4ebdcae5ff0b88cbeb71d00b24ffb320e04d0fa
Milo-Shen <Milo-Shen@outlook.com>
2023-01-01 14:43:04 +0800

    feat: init copyBooks

---
+++ Copy Books/Javascript/solution_1.js
@@ -0,0 +1,9 @@
+// https://www.lintcode.com/problem/437/
+
+const copyBooks = function (pages, k) {
+  // write your code here
+};
+
+const pages = [3, 2, 4];
+const k = 2;
+console.log(copyBooks(pages, k));
