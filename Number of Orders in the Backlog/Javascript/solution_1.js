07ea9d7d0e536a6de77f1bcf0597350f68f9a3db
Milo-Shen <Milo-Shen@outlook.com>
2023-01-02 10:31:24 +0800

    feat: init Number of Orders in the Backlog

---
+++ Number of Orders in the Backlog/Javascript/solution_1.js
@@ -0,0 +1,6 @@
+// https://leetcode.cn/problems/number-of-orders-in-the-backlog/
+
+const { MinHeap } = require('../../Base/MinHeap/Javascript/MinHeap');
+const { MaxHeap } = require('../../Base/MaxHeap/Javascript/MaxHeap');
+
+const getNumberOfBacklogOrders = function (orders) {};
