78f023f6c48c2bcb2abdb37aee9245569179c608
Milo-Shen <Milo-Shen@outlook.com>
2023-01-06 14:13:10 +0800

    feat: init rust solution for median-of-two-sorted-arrays

---
+++ Median of Two Sorted Arrays/Rust/src/main.rs
@@ -0,0 +1,11 @@
+// https://leetcode.cn/problems/median-of-two-sorted-arrays/
+// https://www.lintcode.com/problem/65/
+
+mod solution_1;
+
+fn main() {
+    let nums1 = vec![1, 3];
+    let nums2 = vec![2];
+    let answer = solution_1::find_median_sorted_arrays(nums1, nums2);
+    println!("answer = {}", answer);
+}
---
+++ Median of Two Sorted Arrays/Rust/src/solution_1.rs
@@ -0,0 +1,6 @@
+// https://leetcode.cn/problems/median-of-two-sorted-arrays/
+// https://www.lintcode.com/problem/65/
+
+pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+    0 as f64
+}
