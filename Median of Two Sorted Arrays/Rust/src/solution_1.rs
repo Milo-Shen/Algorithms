b29c6f22de8441d1b17bc25397f7a03f01f81f77
Milo-Shen <Milo-Shen@outlook.com>
2023-01-07 14:25:30 +0800

    feat: add rust solution for median-of-two-sorted-arrays

--- Median of Two Sorted Arrays/Rust/src/solution_1.rs
+++ Median of Two Sorted Arrays/Rust/src/solution_1.rs
@@ -2,5 +2,17 @@
 // https://www.lintcode.com/problem/65/
 
 pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-    0 as f64
+    let mut num = nums1;
+    let mut nums2 = nums2;
+    num.append(&mut nums2);
+    num.sort();
+
+    let len = num.len();
+    let half = (len - 1) / 2;
+
+    return if len % 2 == 0 {
+        (num[half] as f64 + num[half + 1] as f64) / 2.0
+    } else {
+        num[half] as f64
+    };
 }
