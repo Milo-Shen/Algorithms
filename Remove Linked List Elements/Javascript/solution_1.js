c7623d993f1c40357d72705d12dc6e4b9bdf3046
Milo-Shen <Milo-Shen@outlook.com>
2023-01-08 16:42:11 +0800

    feat: add js solution 1 for remove-linked-list-elements

---
+++ Remove Linked List Elements/Javascript/solution_1.js
@@ -0,0 +1,15 @@
+// https://leetcode.cn/problems/remove-linked-list-elements/
+
+const removeElements = function (head, val) {
+  if (!head) {
+    return head;
+  }
+
+  head.next = removeElements(head.next, val);
+  return head.val === val ? head.next : head;
+};
+
+// test cases
+const { build_list, print_list, ListNode } = require('../../Base/List/Javascript/List');
+let head = build_list([1, 2, 2, 1]);
+print_list(removeElements(head, 2));
