1d2091c93d3c78d3336da617bc4d92334a80d100
Milo-Shen <Milo-Shen@outlook.com>
2023-01-14 20:48:34 +0800

    feat: add rust solution 1 for how-many-numbers-are-smaller-than-the-current-number

--- How Many Numbers Are Smaller Than the Current Number/Rust/src/solution_1.rs
+++ How Many Numbers Are Smaller Than the Current Number/Rust/src/solution_1.rs
@@ -1,3 +1,18 @@
 pub fn smaller_numbers_than_current(nums: Vec<i32>) -> Vec<i32> {
-    Vec::new()
+    let len = nums.len();
+    let mut result = Vec::with_capacity(len);
+
+    for i in 0..len {
+        let mut count = 0;
+
+        for j in 0..len {
+            if nums[j] < nums[i] {
+                count += 1;
+            }
+        }
+
+        result.push(count);
+    }
+
+    result
 }
