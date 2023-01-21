bd251472709f065b230acc20d7af8db21be5852d
Milo-Shen <Milo-Shen@outlook.com>
2023-01-21 22:39:03 +0800

    feat: add rust solution for difference-between-element-sum-and-digit-sum-of-an-array

---
+++ Difference Between Element Sum and Digit Sum of an Array/Rust/src/main.rs
@@ -0,0 +1,8 @@
+// https://leetcode.cn/problems/difference-between-element-sum-and-digit-sum-of-an-array/
+
+mod solution_1;
+
+fn main() {
+    let nums = vec![1, 15, 6, 3];
+    println!("answer = {}", solution_1::difference_of_sum(nums));
+}
---
+++ Difference Between Element Sum and Digit Sum of an Array/Rust/src/solution_1.rs
@@ -0,0 +1,15 @@
+pub fn difference_of_sum(nums: Vec<i32>) -> i32 {
+    let mut num_sum = 0;
+    let mut ele_sum = 0;
+
+    for &num in nums.iter() {
+        let mut num = num;
+        ele_sum += num;
+        while num != 0 {
+            num_sum += num % 10;
+            num = num / 10;
+        }
+    }
+
+    return (num_sum - ele_sum).abs();
+}
