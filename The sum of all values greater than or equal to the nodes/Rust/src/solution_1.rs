0af34c2f352325e97132f64dab070f9516ad6172
Milo-Shen <Milo-Shen@outlook.com>
2023-01-22 17:32:04 +0800

    feat: add rust solution 1 for The sum of all values greater than or equal to the nodes

--- The sum of all values greater than or equal to the nodes/Rust/src/solution_1.rs
+++ The sum of all values greater than or equal to the nodes/Rust/src/solution_1.rs
@@ -5,5 +5,24 @@ use std::rc::Rc;
 use std::cell::RefCell;
 
 pub fn convert_bst(root: Option<Rc<RefCell<TreeNode<i32>>>>) -> Option<Rc<RefCell<TreeNode<i32>>>> {
-    None
+    let mut sum = 0;
+
+    dfs(&root, &mut sum);
+
+    root
+}
+
+fn dfs(root: &Option<Rc<RefCell<TreeNode<i32>>>>, sum: &mut i32) {
+    if root.is_none() {
+        return;
+    }
+
+    let root = root.as_ref().unwrap();
+
+    dfs(&root.borrow().right, sum);
+    
+    *sum = *sum + &root.borrow().val;
+    root.borrow_mut().val = *sum;
+
+    dfs(&root.borrow().left, sum);
 }
