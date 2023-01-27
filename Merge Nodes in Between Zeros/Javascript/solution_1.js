01556d31f6744f72c97dec11bc6d626163d34850
Milo-Shen <Milo-Shen@outlook.com>
2023-01-27 15:13:52 +0800

    feat: add js solution 1 for Merge Nodes in Between Zeros

--- Merge Nodes in Between Zeros/Javascript/solution_1.js
+++ Merge Nodes in Between Zeros/Javascript/solution_1.js
@@ -1,24 +1,31 @@
 // https://leetcode.cn/problems/merge-nodes-in-between-zeros/
 
 const mergeNodes = function (head) {
-  let node = head;
   let sum = 0;
+  let node = head;
+  let pre_node = head;
 
   while (node !== null) {
     if (node.val === 0) {
-      console.log(sum);
-      sum = 0;
-    }
+      node.val = sum;
 
-    sum += node.val;
+      if (node !== head) {
+        pre_node.next = node;
+        pre_node = node;
+      }
+
+      sum = 0;
+    } else {
+      sum += node.val;
+    }
 
     node = node.next;
   }
 
-  return head;
+  return head.next;
 };
 
 // test cases
 const { build_list, print_list } = require('../../Base/List/Javascript/List');
-let root = mergeNodes(build_list([0, 3, 1, 0, 4, 5, 2, 0]));
+let root = mergeNodes(build_list([0, 1, 0, 3, 0, 2, 2, 0]));
 print_list(root);
