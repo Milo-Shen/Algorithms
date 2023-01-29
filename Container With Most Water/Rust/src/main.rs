0f4165fbd25d20afc6b8c2d1362a07639d878818
Milo-Shen <Milo-Shen@outlook.com>
2023-01-29 14:14:43 +0800

    feat: init rust for container-with-most-water

---
+++ Container With Most Water/Rust/src/main.rs
@@ -0,0 +1,7 @@
+// https://leetcode.cn/problems/container-with-most-water/
+
+mod solution_1;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ Container With Most Water/Rust/src/solution_1.rs
@@ -0,0 +1,5 @@
+// https://leetcode.cn/problems/container-with-most-water/
+
+pub fn max_area(height: Vec<i32>) -> i32 {
+    0
+}
