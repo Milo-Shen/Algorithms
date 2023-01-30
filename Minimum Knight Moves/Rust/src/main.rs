2dfa1e694d4d5b98ee00189f570867254b987b62
Milo-Shen <Milo-Shen@outlook.com>
2023-01-30 11:30:12 +0800

    feat: init rust for Minimum Knight Moves

---
+++ Minimum Knight Moves/Rust/src/main.rs
@@ -0,0 +1,8 @@
+// https://leetcode.cn/problems/minimum-knight-moves/
+
+mod solution_1;
+
+fn main() {
+    let answer = solution_1::min_knight_moves(5, 5);
+    println!("answer = {}", answer);
+}
---
+++ Minimum Knight Moves/Rust/src/solution_1.rs
@@ -0,0 +1,5 @@
+// https://leetcode.cn/problems/minimum-knight-moves/
+
+pub fn min_knight_moves(x: i32, y: i32) -> i32 {
+    0
+}
