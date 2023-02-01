a394f8830c2d2e079c201d975b89b46c08a54df3
Milo-Shen <Milo-Shen@outlook.com>
2023-02-01 11:14:39 +0800

    feat: init rust for Largest Number At Least Twice of Others

---
+++ Largest Number At Least Twice of Others/Rust/src/main.rs
@@ -0,0 +1,6 @@
+mod solution_1;
+
+fn main() {
+    let answer = solution_1::dominant_index(vec![0, 0, 3, 2]);
+    println!("answer = {}", answer);
+}
---
+++ Largest Number At Least Twice of Others/Rust/src/solution_1.rs
@@ -0,0 +1,5 @@
+// https://leetcode.cn/problems/largest-number-at-least-twice-of-others/
+
+pub fn dominant_index(nums: Vec<i32>) -> i32 {
+    0
+}
