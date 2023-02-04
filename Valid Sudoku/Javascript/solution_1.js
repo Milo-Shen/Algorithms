5772d1d0882064d2c4cbe1f8ac262bfa4a5ebe45
Milo-Shen <Milo-Shen@outlook.com>
2023-02-04 21:28:02 +0800

    feat: add js solution 1 for valid-sudoku

--- Valid Sudoku/Javascript/solution_1.js
+++ Valid Sudoku/Javascript/solution_1.js
@@ -1,8 +1,64 @@
 // https://leetcode.cn/problems/sudoku-solver/
 
-const solveSudoku = function (board) {};
+const isValidSudoku = function (board) {
+  // 每一行是否出现当前数字
+  let row_sets = Array(9);
 
-function is_valid(board, x, y) {}
+  // 每一列上是否出现当前数字
+  let col_sets = Array(9);
+
+  // 3x3 九宫格内是否只出现一次当前数字
+  let area_sets = Array(9);
+
+  for (let i = 0; i < 9; i++) {
+    row_sets[i] = new Map();
+    col_sets[i] = new Map();
+    for (let j = 0; j < 9; j++) {
+      // 初始化 row_sets
+      if (board[i][j] !== '.') {
+        let count = row_sets[i].get(board[i][j]) || 0;
+        row_sets[i].set(board[i][j], count + 1);
+      }
+
+      // 初始化 col_sets
+      if (board[j][i] !== '.') {
+        let count = col_sets[i].get(board[j][i]) || 0;
+        col_sets[i].set(board[j][i], count + 1);
+      }
+
+      // 初始化 3x3
+      let pos = ~~(i / 3) * 3 + ~~(j / 3);
+      area_sets[pos] = area_sets[pos] || new Map();
+      if (board[i][j] !== '.') {
+        let count = area_sets[pos].get(board[i][j]) || 0;
+        area_sets[pos].set(board[i][j], count + 1);
+      }
+    }
+  }
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      let num = board[i][j];
+      // 检查当前行
+      if (row_sets[i].get(num) > 1) {
+        return false;
+      }
+
+      // 检查当前列
+      if (col_sets[j].get(num) > 1) {
+        return false;
+      }
+
+      // 检查 3x3
+      let pos = ~~(i / 3) * 3 + ~~(j / 3);
+      if (area_sets[pos].get(num) > 1) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
 
 let boards = [
   ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
@@ -15,4 +71,4 @@ let boards = [
   ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
   ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
 ];
-console.log(solveSudoku(boards));
+console.log(isValidSudoku(boards));
