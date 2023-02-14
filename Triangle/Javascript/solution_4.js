a5c78b9cd7d486cdfd008ce59b51e72e5ef31311
Milo-Shen <Milo-Shen@outlook.com>
2023-02-14 16:21:22 +0800

    feat: enhance comments for Triangle

--- Triangle/Javascript/solution_4.js
+++ Triangle/Javascript/solution_4.js
@@ -1,6 +1,24 @@
 // https://leetcode.cn/problems/IlPe0q/
 // https://www.lintcode.com/problem/109/
 
+// 递归四要素 vs 动规四要素
+
+// 动规的状态State —— 递归的定义
+// - 用f[i] 或者f[i][j] 代表在某些特定条件下某个规模更小的问题的答案
+// - 规模更小用参数i,j 之类的来划定
+
+// 动规的方程Function —— 递归的拆解
+// - 大问题如何拆解为小问题
+// - f[i][j] = 通过规模更小的一些状态求max / min / sum / or 来进行推导
+
+// 动规的初始化Initialize —— 递归的出口
+// - 设定无法再拆解的极限小的状态下的值
+// - 如f[i][0] 或者f[0][i]
+
+// 动规的答案Answer —— 递归的调用
+// - 最后要求的答案是什么
+// - 如f[n][m] 或者max(f[n][0], f[n][1] ... f[n][m])
+
 // 自底向上进行 dp
 const minimumTotal = function (triangle) {
   let n = triangle.length;
