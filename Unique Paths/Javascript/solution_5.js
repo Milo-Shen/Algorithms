3ff0cef4bcfcf80e55fe3535ee68c19e652a723b
Milo-Shen <Milo-Shen@outlook.com>
2023-02-20 21:10:09 +0800

    feat: add js solution 5 for unique-paths

---
+++ Unique Paths/Javascript/solution_5.js
@@ -0,0 +1,30 @@
+// https://leetcode.cn/problems/unique-paths/
+// https://www.lintcode.com/problem/114/
+
+// 自底向上进行 DP
+
+const uniquePaths = function (m, n) {
+  let dp = [];
+
+  for (let i = 0; i < m; i++) {
+    dp.push(Array(n).fill(0));
+  }
+
+  for (let i = 0; i < n; i++) {
+    dp[0][i] = 1;
+  }
+
+  for (let i = 0; i < m; i++) {
+    dp[i][0] = 1;
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  return dp[m - 1][n - 1];
+};
+
+console.log(uniquePaths(3, 7));
