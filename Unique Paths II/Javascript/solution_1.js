fdb41ed361bc0eb1468489326b3d90b32de703cb
Milo-Shen <Milo-Shen@outlook.com>
2023-02-21 22:05:08 +0800

    feat: init Unique Paths II

---
+++ Unique Paths II/Javascript/solution_1.js
@@ -0,0 +1,11 @@
+// https://leetcode.cn/problems/unique-paths-ii/
+
+const uniquePathsWithObstacles = function (obstacleGrid) {};
+
+console.log(
+  uniquePathsWithObstacles([
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0],
+  ]),
+);
