d2ed8d0d0c8311c657f19d12107fb3a417573b2a
Milo-Shen <Milo-Shen@outlook.com>
2023-02-21 22:22:43 +0800

    feat: add todo to unique-paths-ii

--- Unique Paths II/Javascript/solution_1.js
+++ Unique Paths II/Javascript/solution_1.js
@@ -1,11 +1,60 @@
 // https://leetcode.cn/problems/unique-paths-ii/
 
-const uniquePathsWithObstacles = function (obstacleGrid) {};
+// todo
+const uniquePathsWithObstacles = function (obstacleGrid) {
+  if (!obstacleGrid || !obstacleGrid.length || !obstacleGrid[0] || !obstacleGrid[0].length) {
+    return 0;
+  }
+
+  if (obstacleGrid[0][0] === 1) {
+    return 0;
+  }
+
+  let m = obstacleGrid.length;
+  let n = obstacleGrid[0].length;
+
+  let dp = [];
+
+  for (let i = 0; i < m; i++) {
+    dp.push(Array(n).fill(0));
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (obstacleGrid[0][i] === 1) {
+      dp[0][i] = 0;
+      continue;
+    }
+
+    dp[0][i] = 1;
+  }
+
+  for (let i = 0; i < m; i++) {
+    if (obstacleGrid[i][0] === 1) {
+      dp[i][0] = 0;
+      continue;
+    }
+
+    dp[i][0] = 1;
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      if (obstacleGrid[i][j] === 1) {
+        dp[i][j] = 0;
+        continue;
+      }
+
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  return dp[m - 1][n - 1];
+};
 
 console.log(
   uniquePathsWithObstacles([
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 0, 0],
+    [0, 0],
+    [1, 1],
+    [0, 0],
   ]),
 );
