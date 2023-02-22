5c91bc4f8b1c276dbe0d3ceac450da8a13f29c99
Milo-Shen <Milo-Shen@outlook.com>
2023-02-22 22:16:22 +0800

    feat: init shortest_path2

---
+++ Knight Shortest Path II/Rust/src/main.rs
@@ -0,0 +1,10 @@
+// https://www.lintcode.com/problem/630
+// lint code 上这题输入的 grid 是 boolean 数组, source 和 destination 是 Point 实例, 这个需要注意
+// 另外一个则是 grid 是 boolean 数组，而非 0, 1 数组，和题目介绍不一样
+
+
+mod solution_1;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ Knight Shortest Path II/Rust/src/solution_1.rs
@@ -0,0 +1,7 @@
+// https://www.lintcode.com/problem/630
+// lint code 上这题输入的 grid 是 boolean 数组, source 和 destination 是 Point 实例, 这个需要注意
+// 另外一个则是 grid 是 boolean 数组，而非 0, 1 数组，和题目介绍不一样
+
+pub fn shortest_path2(grid: Vec<Vec<bool>>) -> i32 {
+    0
+}
