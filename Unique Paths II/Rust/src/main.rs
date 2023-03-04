dd05b486e20b55db7d8d75f832a29367cdd8eb1e
Milo-Shen <Milo-Shen@outlook.com>
2023-03-04 14:46:12 +0800

    feat: update rust solution for Unique Paths II

--- Unique Paths II/Rust/src/main.rs
+++ Unique Paths II/Rust/src/main.rs
@@ -1,6 +1,7 @@
 // https://leetcode.cn/problems/unique-paths-ii/
 
 mod solution_1;
+mod solution_2;
 
 fn main() {
     let grid = vec![
@@ -9,6 +10,9 @@ fn main() {
         vec![0, 0],
     ];
 
-    let answer = solution_1::unique_paths_with_obstacles(grid);
+    let answer = solution_1::unique_paths_with_obstacles(grid.clone());
+    println!("answer = {}", answer);
+
+    let answer = solution_2::unique_paths_with_obstacles(grid);
     println!("answer = {}", answer);
 }
--- Unique Paths II/Rust/src/solution_2.rs
+++ Unique Paths II/Rust/src/solution_2.rs
@@ -21,20 +21,18 @@ pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
 
     for i in 1..n {
         if obstacle_grid[0][i] == 1 {
-            dp[0][i] = 0;
-            continue;
+            break;
         }
 
-        dp[0][i] = if dp[0][i - 1] == 0 { 0 } else { 1 };
+        dp[0][i] = 1;
     }
 
     for i in 1..m {
         if obstacle_grid[i][0] == 1 {
-            dp[i][0] = 0;
-            continue;
+            break;
         }
 
-        dp[i][0] = if dp[i - 1][0] == 0 { 0 } else { 1 };
+        dp[i][0] = 1;
     }
 
     for i in 1..m {
