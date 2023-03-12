05d8118f59473112296e64dfda5d8f39c5e93cc8
Milo-Shen <Milo-Shen@outlook.com>
2023-03-12 14:01:00 +0800

    feat: add js solution 1 for jump-game-vi

--- Jump Game VI/Javascript/solution_1.js
+++ Jump Game VI/Javascript/solution_1.js
@@ -1,5 +1,19 @@
 // https://leetcode.cn/problems/jump-game-vi/
 
-const maxResult = function (nums, k) {};
+const maxResult = function (nums, k) {
+  let len = nums.length;
 
-console.log(maxResult([1, -1, -2, 4, -7, 3], 2));
+  let dp = Array(len).fill(-Infinity);
+  dp[0] = nums[0];
+
+  for (let i = 1; i < len; i++) {
+    for (let j = Math.max(0, i - k); j < i; j++) {
+      dp[i] = Math.max(dp[i], dp[j]);
+    }
+    dp[i] += nums[i];
+  }
+
+  return dp[len - 1];
+};
+
+console.log(maxResult([10, -5, -2, 4, 0, 3], 2));
