7e35afecd496b473849bc3c8528c87361c8807de
Milo-Shen <Milo-Shen@outlook.com>
2023-05-02 15:20:31 +0800

    feat: update comment for find-bottom-left-tree-value

--- Find Bottom Left Tree Value/Javascript/solution.js
+++ Find Bottom Left Tree Value/Javascript/solution.js
@@ -1,5 +1,8 @@
 // https://leetcode.cn/problems/find-bottom-left-tree-value/
 
+// 广度优先搜索遍历每一层的节点。
+// 在遍历一个节点时，需要先把它的非空右子节点放入队列，然后再把它的非空左子节点放入队列，这样才能保证从右到左遍历每一层的节点。
+// 广度优先搜索所遍历的最后一个节点的值就是最底层最左边节点的值。
 function findBottomLeftValue(root) {
   // 逆序 BFS 二叉树
   let queue = [root];
