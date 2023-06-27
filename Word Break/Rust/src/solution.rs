a8117cbb091699bffe6e1293e6359f4a4f2c1d75
Milo-Shen <Milo-Shen@outlook.com>
2023-06-27 22:38:45 +0800

    feat: add rust solution 1 for word-break

--- Word Break/Rust/src/solution.rs
+++ Word Break/Rust/src/solution.rs
@@ -1,15 +1,22 @@
 // https://leetcode.cn/problems/word-break/
-
 use std::collections::HashSet;
 
 pub fn word_break(s: String, word_dict: Vec<String>) -> bool {
-    let n = word_dict.len();
+    let n = s.len();
 
-    if n == 0 {
+    if n == 0 || word_dict.len() == 0 {
         return false;
     }
 
     let mut word_set: HashSet<String> = word_dict.into_iter().collect();
+    let mut dp = vec![false; n + 1];
+    dp[0] = true;
 
-    false
+    for i in 1..=n {
+        for j in 0..i {
+            dp[i] = dp[i] || (dp[j] && word_set.contains(&s[j..i].to_string()));
+        }
+    }
+
+    dp[n]
 }
