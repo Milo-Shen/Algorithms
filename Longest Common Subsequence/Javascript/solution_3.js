f2f9c391de0a1e28abc61a70edb6744f8b14d8a5
Milo-Shen <Milo-Shen@outlook.com>
2023-10-05 20:52:33 +0800

    feat: add js solution 3 for longestCommonSubsequence

---
+++ Longest Common Subsequence/Javascript/solution_3.js
@@ -0,0 +1,37 @@
+// https://leetcode.cn/problems/longest-common-subsequence/description/
+const longestCommonSubsequence = function (text1, text2) {
+  if (!text1 || !text2) {
+    return 0;
+  }
+
+  let n = text1.length;
+  let m = text2.length;
+
+  let dp = [];
+  for (let i = 0; i < 2; i++) {
+    dp.push(Array(m + 1).fill(0));
+  }
+
+  for (let i = 0; i < n + 1; i++) {
+    dp[i % 2][0] = 0;
+  }
+
+  for (let i = 0; i < m + 1; i++) {
+    dp[0][i] = 0;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    dp[i % 2][0] = 0;
+
+    for (let j = 1; j <= m; j++) {
+      dp[i % 2][j] = Math.max(dp[(i - 1) % 2][j], dp[i % 2][j - 1]);
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i % 2][j] = Math.max(dp[i % 2][j], dp[(i - 1) % 2][j - 1] + 1);
+      }
+    }
+  }
+
+  return dp[n % 2][m];
+};
+
+console.log(longestCommonSubsequence('abcde', 'ace'));
