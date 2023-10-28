12f95613125f0c31d073e107d1a92de6683f0dc7
Milo-Shen <Milo-Shen@outlook.com>
2023-10-28 20:18:19 +0800

    feat: init findNumberOfLIS

---
+++ Number of Longest Increasing Subsequence/Javascript/solution_1.js
@@ -0,0 +1,5 @@
+// https://leetcode.cn/problems/number-of-longest-increasing-subsequence/description/
+
+const findNumberOfLIS = function (nums) {};
+
+console.log(findNumberOfLIS([1, 3, 5, 4, 7]));
