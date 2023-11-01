938c7277f06aec67ea9b457aa4ef02649368169e
Milo-Shen <Milo-Shen@outlook.com>
2023-11-01 20:09:34 +0800

    feat: add issue js solution 1 for largestDivisibleSubset

---
+++ Largest Divisible Subset/Javascript/solution_1.js
@@ -0,0 +1,39 @@
+// https://leetcode.cn/problems/largest-divisible-subset/
+// https://www.lintcode.com/problem/603
+
+// todo: solution has issues
+const largestDivisibleSubset = function (nums) {
+  if (!nums || !nums.length) {
+    return nums;
+  }
+
+  let len = nums.length;
+  let dp = Array(len).fill(1);
+  let prev = Array(len).fill(-1);
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      if ((nums[i] % nums[j] === 0 || nums[j] % nums[i] === 0) && dp[i] < dp[j] + 1) {
+        dp[i] = dp[j] + 1;
+        prev[i] = j;
+      }
+    }
+  }
+
+  let last = 0;
+  for (let i = 0; i < len; i++) {
+    if (dp[last] < dp[i]) {
+      last = i;
+    }
+  }
+
+  let answer = [];
+  while (last !== -1) {
+    answer.unshift(nums[last]);
+    last = prev[last];
+  }
+
+  return answer;
+};
+
+console.log(largestDivisibleSubset([5, 9, 18, 54, 108, 540, 90, 180, 360, 720]));
