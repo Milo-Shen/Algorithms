17712720f2fd6d238eabc1ef7555fc1eea093c3b
Milo-Shen <Milo-Shen@outlook.com>
2023-12-08 08:20:19 +0800

    feat: add js solution 1 for productExceptSelf

--- Product of Array Except Self/Javascript/solution_1.js
+++ Product of Array Except Self/Javascript/solution_1.js
@@ -1,1 +1,23 @@
 // https://leetcode.cn/problems/product-of-array-except-self/description/
+
+const productExceptSelf = function (nums) {
+  let result = [];
+
+  let len = nums.length;
+
+  for (let i = 0; i < len; i++) {
+    let ans = 1;
+
+    for (let j = 0; j < len; j++) {
+      if (i !== j) {
+        ans *= nums[j];
+      }
+    }
+
+    result.push(ans);
+  }
+
+  return result;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4]));
