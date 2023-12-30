9bbe781b53e1c8818518dc27f58441a99b49a60c
Milo-Shen <Milo-Shen@outlook.com>
2023-12-30 22:07:15 +0800

    feat: add solution for trapping-rain-water

--- Trapping Rain Water/Javascript/solution_1.js
+++ Trapping Rain Water/Javascript/solution_1.js
@@ -1,13 +1,46 @@
 // https://leetcode.cn/problems/trapping-rain-water/
 const trap = function (height) {
+  let total = 0;
   let i = 0;
-  let j = 0;
 
   while (height[i] === 0) {
     i++;
   }
 
-  console.log(i);
+  let j = i + 1;
+
+  while (j < height.length) {
+    if (height[j] >= height[i]) {
+      let max_h = Math.min(height[i], height[j]);
+      for (let m = i + 1; m < j; m++) {
+        total = total + max_h - height[m];
+      }
+      i = j;
+    }
+    j++;
+  }
+
+  let peak = i;
+  i = height.length - 1;
+
+  while (height[i] === 0) {
+    i--;
+  }
+
+  j = i - 1;
+
+  while (j >= peak) {
+    if (height[j] >= height[i]) {
+      let max_h = Math.min(height[i], height[j]);
+      for (let m = i - 1; m > j; m--) {
+        total = total + max_h - height[m];
+      }
+      i = j;
+    }
+    j--;
+  }
+
+  return total;
 };
 
-console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+console.log(trap([4, 2, 3]));
