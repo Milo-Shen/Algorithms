1ba5fb38f61dc2ddc55f7f24c04df7dfc0ebc593
Milo-Shen <Milo-Shen@outlook.com>
2024-01-10 08:26:25 +0800

    feat: add js solution 1 for path-sum-iii

--- Path Sum III/Javascript/solution_1.js
+++ Path Sum III/Javascript/solution_1.js
@@ -1,8 +1,47 @@
 // https://leetcode.cn/problems/path-sum-iii/description/
 
-const pathSum = function (root, targetSum) {};
+const pathSum = function (root, targetSum) {
+  if (!root) {
+    return 0;
+  }
 
-const dfs = function (root, sum, targetSum) {};
+  let result = { total: 0 };
+
+  let queue = [root];
+  while (queue.length) {
+    let node = queue.shift();
+
+    dfs(node, node.val, [node.val], targetSum, result);
+
+    if (node.left) {
+      queue.push(node.left);
+    }
+
+    if (node.right) {
+      queue.push(node.right);
+    }
+  }
+
+  return result.total;
+};
+
+const dfs = function (root, sum, path, targetSum, result) {
+  if (!root) {
+    return;
+  }
+
+  if (path.reduce((a, b) => a + b) === targetSum) {
+    result.total++;
+  }
+
+  if (root.left) {
+    dfs(root.left, sum + root.left.val, [...path, root.left.val], targetSum, result);
+  }
+
+  if (root.right) {
+    dfs(root.right, sum + root.right.val, [...path, root.right.val], targetSum, result);
+  }
+};
 
 // test cases
 const { buildBinaryTree } = require('../../Base/BinaryTree/Javascript/BinaryTree');
