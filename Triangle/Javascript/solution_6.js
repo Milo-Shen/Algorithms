cd82630daf1e1793d7a4836f2258defa7dbc02a3
Milo-Shen <Milo-Shen@outlook.com>
2024-01-17 16:34:23 +0800

    feat: update js solution 6 for minimumTotal

--- Triangle/Javascript/solution_6.js
+++ Triangle/Javascript/solution_6.js
@@ -5,22 +5,22 @@ const minimumTotal = function (triangle) {
   let n = triangle.length;
 
   let dp = [];
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < 2; i++) {
     dp.push(Array(n).fill(0));
   }
 
   dp[0][0] = triangle[0][0];
 
   for (let i = 1; i < n; i++) {
-    dp[i][0] = dp[i - 1][0] + triangle[i][0];
-    dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+    dp[i % 2][0] = dp[(i - 1) % 2][0] + triangle[i][0];
+    dp[i % 2][i] = dp[(i - 1) % 2][i - 1] + triangle[i][i];
 
     for (let j = 1; j < i; j++) {
-      dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j];
+      dp[i % 2][j] = Math.min(dp[(i - 1) % 2][j], dp[(i - 1) % 2][j - 1]) + triangle[i][j];
     }
   }
 
-  return Math.min(...dp[n - 1]);
+  return Math.min(...dp[(n - 1) % 2]);
 };
 
 let triangle = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]];
