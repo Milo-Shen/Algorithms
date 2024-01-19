9abbdae950a739100e38cae74d4edc153d745e41
Milo-Shen <Milo-Shen@outlook.com>
2024-01-19 19:41:48 +0800

    feat: enhance js solution 2 for Longest Increasing Subsequence

---
+++ Longest Increasing Subsequence/Javascript/solution_2.js
@@ -0,0 +1,39 @@
+// https://leetcode.cn/problems/longest-increasing-subsequence/
+// https://www.lintcode.com/problem/76/
+
+const lengthOfLIS = function (nums) {
+  if (!nums || !nums.length) {
+    return 0;
+  }
+
+  let len = nums.length;
+  let dp = Array(len).fill(1);
+  let prev = Array(len).fill(-1);
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+        prev[i] = j;
+      }
+    }
+  }
+
+  let longest = [];
+
+  let last = 0;
+  for (let i = 0; i < len; i++) {
+    if (dp[i] > dp[last]) {
+      last = i;
+    }
+  }
+
+  while (last !== -1) {
+    longest.unshift(nums[last]);
+    last = prev[last];
+  }
+
+  return longest;
+};
+
+console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
