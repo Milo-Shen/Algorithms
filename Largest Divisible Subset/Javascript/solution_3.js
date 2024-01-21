f53481ae110e9a2c1b61c934b6b840412b15f5dd
Milo-Shen <Milo-Shen@outlook.com>
2024-01-21 18:06:50 +0800

    feat: update solution 3 for largestDivisibleSubset

--- Largest Divisible Subset/Javascript/solution_3.js
+++ Largest Divisible Subset/Javascript/solution_3.js
@@ -8,12 +8,64 @@ const largestDivisibleSubset = function (nums) {
 
   nums = nums.sort((a, b) => a - b);
 
-  let len = nums.length;
-  let last = 0;
   let dp = new Map();
   let prev = new Map();
+
+  for (let num of nums) {
+    dp.set(num, 1);
+    prev.set(num, -1);
+  }
+
+  let last_num = nums[0];
+
+  for (let num of nums) {
+    for (let factor of getFactors(num)) {
+      if (!dp.has(factor)) {
+        continue;
+      }
+
+      if (dp.get(num) < dp.get(factor) + 1) {
+        dp.set(num, dp.get(factor) + 1);
+        prev.set(num, factor);
+      }
+    }
+
+    if (dp.get(num) > dp.get(last_num)) {
+      last_num = num;
+    }
+  }
+
+  let answer = [];
+
+  while (last_num !== -1) {
+    answer.unshift(last_num);
+    last_num = prev.get(last_num);
+  }
 
   return answer;
 };
 
-console.log(largestDivisibleSubset([5, 9, 18, 54, 108, 540, 90, 180, 360, 720]));
+const getFactors = function (num) {
+  if (num === 1) {
+    return [];
+  }
+
+  let factors = [];
+  let factor = 1;
+
+  while (factor * factor <= num) {
+    if (num % factor === 0) {
+      factors.push(factor);
+
+      if (factor * factor !== num && factor !== 1) {
+        factors.push(num / factor);
+      }
+    }
+
+    factor += 1;
+  }
+
+  return factors;
+};
+
+console.log(largestDivisibleSubset([1, 2, 3]));
