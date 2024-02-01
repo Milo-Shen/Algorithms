3651553f4a00db62dbfe3c8a8a74b9429a3e53f1
Milo-Shen <Milo-Shen@outlook.com>
2024-02-01 17:42:12 +0800

    feat: add js solution for isMatch

--- Wildcard Matching/Javascript/solution_1.js
+++ Wildcard Matching/Javascript/solution_1.js
@@ -1,3 +1,43 @@
 // https://leetcode.cn/problems/wildcard-matching/description/
 
-const isMatch = function (s, p) {};
+const isMatch = function (s, p) {
+  let n = s.length;
+  let m = p.length;
+
+  let dp = [];
+  for (let i = 0; i < n + 1; i++) {
+    dp.push(Array(m + 1).fill(false));
+  }
+
+  dp[0][0] = true;
+
+  for (let i = 1; i <= n; i++) {
+    dp[i][0] = false;
+  }
+
+  for (let i = 0; i < m; i++) {
+    dp[i + 1][0] = dp[i][0] && p[i + 1] === '*';
+  }
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= m; i++) {
+      if (p[j - 1] === '*') {
+        dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+      } else {
+        dp[i][j] = dp[i - 1][j - 1] && isMatch(s[i - 1], p[j - 1]);
+      }
+    }
+  }
+
+  return dp[n][m];
+};
+
+const isMatchChar = function (s, p) {
+  if (p === '?') {
+    return true;
+  }
+
+  return s === p;
+};
+
+console.log(isMatch('abc', '*?c'));
