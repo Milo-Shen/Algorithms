2c23da57c1d47caf72ef39a7f818c5e1ee847495
Milo-Shen <Milo-Shen@outlook.com>
2024-02-04 13:58:52 +0800

    feat: add js solution 1 for numDecodings

---
+++ Decode Ways/Javascript/solution_1.js
@@ -0,0 +1,21 @@
+// https://leetcode.cn/problems/decode-ways/description/
+const numDecodings = function (s) {
+  let n = s.length;
+
+  let dp = Array(n + 1).fill(0);
+  dp[0] = 1;
+
+  for (let i = 1; i <= n; i++) {
+    if (s[i - 1] !== '0') {
+      dp[i] += dp[i - 1];
+    }
+
+    if (i > 1 && s[i - 2] !== '0' && (s[i - 2] - '0') * 10 + (s[i - 1] - '0') <= 26) {
+      dp[i] += dp[i - 2];
+    }
+  }
+
+  return dp[n];
+};
+
+console.log(numDecodings('06'));
