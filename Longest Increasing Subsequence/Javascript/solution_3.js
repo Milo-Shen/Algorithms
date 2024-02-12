904492db0da190cf76bf008b7ddd8d30bed4cc5e
Milo-Shen <Milo-Shen@outlook.com>
2024-02-12 11:17:16 +0800

    feat: init solution 3 for lengthOfLIS

---
+++ Longest Increasing Subsequence/Javascript/solution_3.js
@@ -0,0 +1,10 @@
+// https://leetcode.cn/problems/longest-increasing-subsequence/
+// https://www.lintcode.com/problem/76/
+
+const lengthOfLIS = function (nums) {
+  if (!nums || !nums.length) {
+    return 0;
+  }
+};
+
+console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
