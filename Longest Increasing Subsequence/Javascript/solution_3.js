7a0ef3dd683907eef01dc89e0cfb4999eb138cc9
Milo-Shen <Milo-Shen@outlook.com>
2024-02-12 13:39:04 +0800

    feat: add js solution 3 for longest-increasing-subsequence

--- Longest Increasing Subsequence/Javascript/solution_3.js
+++ Longest Increasing Subsequence/Javascript/solution_3.js
@@ -5,6 +5,40 @@ const lengthOfLIS = function (nums) {
   if (!nums || !nums.length) {
     return 0;
   }
+
+  let n = nums.length;
+
+  let lis = Array(n + 1).fill(Infinity);
+  lis[0] = -Infinity;
+
+  let longest = 0;
+  for (let num of nums) {
+    let index = first_gte(lis, num);
+    lis[index] = num;
+    longest = Math.max(longest, index);
+  }
+
+  return longest;
+};
+
+const first_gte = function (nums, target) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start + 1 < end) {
+    let mid = start + Math.floor((end - start) / 2);
+    if (nums[mid] >= target) {
+      end = mid;
+    } else {
+      start = mid;
+    }
+  }
+
+  if (nums[start] >= target) {
+    return start;
+  }
+
+  return end;
 };
 
 console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
